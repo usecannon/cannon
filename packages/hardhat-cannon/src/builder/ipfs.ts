35dc9eb6cbb15a20c28d344f3f4eac225b34f2c3
Matías <mjlescano@protonmail.com>
2022-02-16 10:43:19 -0700

    add ipfs upload fn

---
+++ packages/hardhat-cannon/src/builder/ipfs.ts
@@ -0,0 +1,47 @@
+// Save on IPFS by filter on the given repo:tag folder
+/*
+  Base folder should be /ipfs/cannon/repo/tag
+    * Canonfile.toml
+    * cache (D)
+*/
+
+import fs from 'fs/promises';
+import { create, IPFSHTTPClient } from 'ipfs-http-client';
+
+export interface IPFSFile {
+  remotePath: string
+  localPath: string
+}
+
+export default class IPFS {
+  client: IPFSHTTPClient;
+
+  constructor({ url = 'http://localhost:5001/api/v0' } = {}) {
+    this.client = create({ url });
+  }
+
+  async add(files: IPFSFile[]) {
+    const results = [];
+
+    for await (const result of this.client.addAll(_openFiles(files))) {
+      results.push(result);
+    }
+
+    return results;
+  }
+}
+
+async function _openFile(file: IPFSFile) {
+  const fileHandler = await fs.open(file.localPath, 'r');
+
+  return {
+    path: file.remotePath,
+    content: fileHandler.createReadStream(),
+  };
+}
+
+async function* _openFiles(files: IPFSFile[]) {
+  for (const file of files) {
+    yield _openFile(file);
+  }
+}
