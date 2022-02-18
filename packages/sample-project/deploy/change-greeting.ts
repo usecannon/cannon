2da95c2aec0b5ebf2eb4582c044087124777ba15
Daniel Beal <git@danb.email>
2022-02-18 10:08:06 -0800

    add missing deployment file

---
+++ packages/sample-project/deploy/change-greeting.ts
@@ -0,0 +1,10 @@
+import hre from 'hardhat';
+
+export async function exec(address: string, msg: string) {
+    const contract = await hre.ethers.getContractAt('Greeter', address);
+
+    const txn = await contract.setGreeting(msg);
+    const receipt = await txn.wait();
+
+    return { hash: receipt.transactionHash };
+}
