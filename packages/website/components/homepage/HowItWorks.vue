0b4eba70587be389db254f7c1f152f4800926815
Noah Litvin <noah.litvin@gmail.com>
2022-02-18 14:12:08 -0700

    simplify hp example

--- packages/website/components/homepage/HowItWorks.vue
+++ packages/website/components/homepage/HowItWorks.vue
@@ -15,7 +15,7 @@
         width="100%"
         alignItems="center"
       >
-        <CGridItem :col-span="[12, 6]" :pr="[0, 12]">
+        <CGridItem :col-span="[12, 6]">
           <CHeading
             as="h3"
             size="lg"
@@ -25,7 +25,7 @@
             mb="5"
             >How it works</CHeading
           >
-          <CText :fontSize="['lg', 'xl']" as="p" mb="5">
+          <CText as="p" mb="5">
             <strong>Cannon</strong> is a
             <CLink
               isExternal
@@ -35,22 +35,19 @@
             >
             plug-in inspired by Docker and Terraform.
           </CText>
-          <CText :fontSize="['lg', 'xl']" as="p" mb="3"
+          <CText as="p" mb="3"
             >Configure your protocol's scripts, keepers, and on-chain
             dependencies in your <code>cannon.json</code> file for
-            <strong>automated deployments on local and live chains.</strong>
+            <strong
+              >automated deployments on multiple local and live chains.</strong
+            >
           </CText>
-          <CText :fontSize="['lg', 'xl']" as="p" mb="5">
+          <CText as="p" :mb="[5, 0]">
             Cannon comes with a
             <strong>built-in package manager</strong> (backed on Ethereum and
-            IPFS) so you can easily include existing protocols.</CText
-          >
-          <CText :fontSize="['lg', 'xl']" as="p" :mb="[5, 0]"
-            >For example, if your smart contract depends on a Chainlink keeper
-            and Synthetix across multiple chains, Cannon automates the
-            deployment of a local environment with these contracts for
-            development and testing.</CText
-          >
+            IPFS) so you can easily include existing protocols for development
+            and testing.
+          </CText>
         </CGridItem>
         <CGridItem :col-span="[12, 6]">
           <CText fontWeight="semibold" mb="1"
@@ -80,26 +77,16 @@
           >
 
           <CText fontWeight="semibold" mb="1"
-            >3. Define a <code>cannon.json</code> file</CText
+            >3. Start a local chain with Synthetix</CText
           >
-          <CBox mb="6">
-            <client-only :placeholder="code">
-              <prism-editor
-                class="code-editor"
-                v-model="code"
-                :highlight="highlighter"
-              ></prism-editor>
-            </client-only>
-          </CBox>
-
-          <CText fontWeight="semibold" mb="1">4. Provision local nodes</CText>
           <CCode
             variant-color="black"
             background="black"
             py="1"
             px="3"
             width="100%"
-            >npx hardhat cannon</CCode
+            mb="6"
+            >npx hardhat cannon synthetix:latest</CCode
           >
         </CGridItem>
       </CGrid>
@@ -108,49 +95,8 @@
 </template>
 
 <script lang="js">
-  // import Prism Editor
-  import { PrismEditor } from 'vue-prism-editor';
-  import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
-
-  // import highlighting library (you can use any library you want just return html string)
-  import { highlight, languages } from 'prismjs/components/prism-core';
-  import 'prismjs/components/prism-json';
-  import 'prismjs/themes/prism-dark.css'; // import syntax highlighting styles
-
 export default {
-  name: 'HowItWorks',
-    components: {
-      PrismEditor,
-    },
-    methods: {
-      highlighter(code) {
-        return  highlight(code, languages.json);
-      },
-    },
-  data() {
-    return {
-      code: `{
-    "name": "mySampleProject:latest",
-    "chains": [
-        {
-            "deploy": [
-              "mySampleProject:latest",
-              "synthetix:2.62",
-              "keeper:snapshot-keeper"
-            ],
-            "chainId": 10
-        },
-        {
-            "deploy": [
-              "mySampleProject:latest",
-              "synthetix:2.62"
-            ],
-            "chainId": 100
-        }
-    ]
-}`
-    }
-  }
+  name: 'HowItWorks'
 }
 </script>
 
