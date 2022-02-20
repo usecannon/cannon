fffa05aaf8ce44363771875f5936a3bceba366bd
Daniel Beal <git@danb.email>
2022-02-19 18:49:10 -0800

    add compatibility for tags and changeg of ownership to the registry

--- packages/registry/contracts/CannonRegistry.sol
+++ packages/registry/contracts/CannonRegistry.sol
@@ -5,15 +5,17 @@ contract CannonRegistry {
   error Unauthorized();
   error InvalidUrl();
 
-  event ProtocolPublish(bytes32 name, bytes32 version, string url, address owner);
+  event ProtocolPublish(bytes32 indexed name, bytes32 indexed version, bytes32[] indexed tags, string url, address owner);
 
-  mapping(bytes32 => mapping(bytes32 => string)) urls;
-  mapping(bytes32 => address) owners;
+  mapping(bytes32 => mapping(bytes32 => string)) public urls;
+  mapping(bytes32 => address) public owners;
 
-  bytes32[] protocols;
+  bytes32[] public protocols;
   mapping(bytes32 => bytes32[]) versions;
 
-  function publish(bytes32 _name, bytes32 _version, string memory _url) external {
+  mapping(bytes32 => address) public nominatedOwner;
+
+  function publish(bytes32 _name, bytes32 _version, bytes32[] memory _tags, string memory _url) external {
     if (bytes(_url).length == 0) {
       revert InvalidUrl();
     }
@@ -33,7 +35,30 @@ contract CannonRegistry {
 
     urls[_name][_version] = _url;
 
-    emit ProtocolPublish(_name, _version, _url, msg.sender);
+    for (uint i = 0;i < _tags.length;i++) {
+      urls[_name][_tags[i]] = _url;
+    }
+
+    emit ProtocolPublish(_name, _version, _tags, _url, msg.sender);
+  }
+
+  function nominateNewOwner(bytes32 _name, address _newOwner) external {
+    if (owners[_name] != msg.sender) {
+      revert Unauthorized();
+    }
+
+    nominatedOwner[_name] = _newOwner;
+  }
+
+  function acceptOwnership(bytes32 _name) external {
+    address newOwner = nominatedOwner[_name];
+
+    if (msg.sender != newOwner) {
+      revert Unauthorized();
+    }
+
+    owners[_name] = newOwner;
+    nominatedOwner[_name] = address(0);
   }
 
   function getProtocols() view external returns (bytes32[] memory) {
