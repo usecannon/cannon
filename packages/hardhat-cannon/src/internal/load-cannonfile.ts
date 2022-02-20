9110605822f0b2f1aca56acb7dcf80f1f6ffe944
Matías <mjlescano@protonmail.com>
2022-02-19 18:59:52 -0700

    update name & version validation

--- packages/hardhat-cannon/src/internal/load-cannonfile.ts
+++ packages/hardhat-cannon/src/internal/load-cannonfile.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import toml from '@iarna/toml';
 import { HardhatPluginError } from 'hardhat/plugins';
+import { ethers } from 'ethers';
 
 export default function loadCannonfile(filepath: string) {
   if (!fs.existsSync(filepath)) {
@@ -12,13 +13,17 @@ export default function loadCannonfile(filepath: string) {
 
   const def = toml.parse(fs.readFileSync(filepath).toString('utf8'));
 
-  if (!def.name) {
-    throw new Error('Missing "name" property on cannonfile.toml');
+  if (!def.name || typeof def.name !== 'string') {
+    throw new Error('Invalid "name" property on cannonfile.toml');
   }
 
-  if (!def.version) {
-    throw new Error('Missing "version" property on cannonfile.toml');
+  ethers.utils.formatBytes32String(def.name);
+
+  if (!def.version || typeof def.version !== 'string') {
+    throw new Error('Invalid "version" property on cannonfile.toml');
   }
+
+  ethers.utils.formatBytes32String(def.version);
 
   return def as any;
 }
