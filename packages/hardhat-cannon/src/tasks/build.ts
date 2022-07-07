1f55962ccae9b584e979058ae012a60c8784dbb6
Daniel Beal <git@danb.email>
2022-07-06 20:34:53 -0700

    add hardhat impersonate calls on getSigner for local net

--- packages/hardhat-cannon/src/tasks/build.ts
+++ packages/hardhat-cannon/src/tasks/build.ts
@@ -97,6 +97,9 @@ task(TASK_BUILD, 'Assemble a defined chain and save it to to a state which can b
         baseDir: hre.config.paths.root,
         savedChartsDir: hre.config.paths.cannon,
         async getSigner(addr: string) {
+          // on test network any user can be conjured
+          await provider.send('hardhat_impersonateAccount', [addr]);
+          await provider.send('hardhat_setBalance', [addr, ethers.utils.parseEther('10000').toHexString()]);
           return hre.ethers.getSigner(addr);
         },
 
