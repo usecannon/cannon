c69941a49d3f582a446c8348a5f068676818f23c
Daniel Beal <git@danb.email>
2022-07-12 22:51:35 -0700

    remove node provision step for now

--- packages/sample-project/run-test.sh
+++ packages/sample-project/run-test.sh
@@ -13,9 +13,6 @@ DEBUG=cannon:* npx hardhat cannon:build
 # run a build of the consumer cannonfile
 DEBUG=cannon:* npx hardhat cannon:build --file cannonfile.consumer.toml
 
-# test node provision
-DEBUG=cannon:* npx hardhat cannon consumer:0.0.1 &
-
 while true; do
 	if curl http://localhost:8545/; then break; fi
 	sleep 1;
