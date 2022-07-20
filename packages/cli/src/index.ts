56dea67e7d8b0af59d8d46bcba6f78a730e28125
Matías <mjlescano@protonmail.com>
2022-07-20 14:36:33 +0200

    add run comand

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { red, bold } from 'chalk';
 
 import { checkCannonVersion } from './helpers';
 import pkg from '../package.json';
@@ -14,31 +15,45 @@ program
   .hook('preAction', async function () {
     await checkCannonVersion(pkg.version);
   });
+// .action(function (_, thisCommand) {
+//   const commands = program.commands.map((c) => c.name());
+//   const packageName = thisCommand.args[0];
 
-program
-  .command('run')
-  .description('Utility for instantly loading cannon packages in standalone contexts.')
-  .usage('[global options] <name>:<semver> [key=value]')
-  .argument('<package>', 'Label and version of the cannon package to load')
-  .argument('[settings...]', 'Arguments used to modify the given package')
-  .option('-h --host <name>', 'Host which the JSON-RPC server will be exposed')
-  .option('-p --port <number>', 'Port which the JSON-RPC server will be exposed')
-  .option('-f --fork <url>', 'Fork the network at the specified RPC url')
-  .option('--logs', 'Show RPC logs instead of interact prompt. If unspecified, defaults to an interactive terminal.')
-  .option('--preset <name>', 'Load an alternate setting preset (default: main)')
-  .option('--write-deployments <path>', 'Path to write the deployments data (address and ABIs), like "./deployments"')
-  .option('-e --exit', 'Exit after building')
-  .option('--registry-rpc <url>', 'URL to use for eth JSON-RPC endpoint', 'https://cloudflare-eth.com/v1/mainnet')
-  .option(
-    '--registry-address <address>',
-    'Address where the cannon registry is deployed',
-    '0xA98BE35415Dd28458DA4c1C034056766cbcaf642'
-  )
-  .option('--ipfs-url <https://...>', 'Host to pull IPFS resources from', 'https://usecannon.infura-ipfs.io')
-  .action(async function (packageName, settings, options, program) {
-    const { default: command } = await import('./commands/run');
-    await command(packageName, settings, options, program);
-  });
+//   if (packageName && !commands.includes(thisCommand.name)) {
+//     console.error(red(`"${packageName}" isn't a command recognized by cannon.`));
+//     console.error(red(`Did you mean ${bold(`run ${packageName}`)} instead of ${packageName}?`));
+//     process.exit(1);
+//   }
+// });
+
+configureRun(program);
+configureRun(program.command('run'));
+
+function configureRun(program: Command) {
+  return program
+    .description('Utility for instantly loading cannon packages in standalone contexts.')
+    .usage('[global options] <name>:<semver> [key=value]')
+    .argument('<package>', 'Label and version of the cannon package to load')
+    .argument('[settings...]', 'Arguments used to modify the given package')
+    .option('-h --host <name>', 'Host which the JSON-RPC server will be exposed')
+    .option('-p --port <number>', 'Port which the JSON-RPC server will be exposed')
+    .option('-f --fork <url>', 'Fork the network at the specified RPC url')
+    .option('--logs', 'Show RPC logs instead of interact prompt. If unspecified, defaults to an interactive terminal.')
+    .option('--preset <name>', 'Load an alternate setting preset (default: main)')
+    .option('--write-deployments <path>', 'Path to write the deployments data (address and ABIs), like "./deployments"')
+    .option('-e --exit', 'Exit after building')
+    .option('--registry-rpc <url>', 'URL to use for eth JSON-RPC endpoint', 'https://cloudflare-eth.com/v1/mainnet')
+    .option(
+      '--registry-address <address>',
+      'Address where the cannon registry is deployed',
+      '0xA98BE35415Dd28458DA4c1C034056766cbcaf642'
+    )
+    .option('--ipfs-url <https://...>', 'Host to pull IPFS resources from', 'https://usecannon.infura-ipfs.io')
+    .action(async function (packageName, settings, options, program) {
+      const { default: command } = await import('./commands/run');
+      await command(packageName, settings, options, program);
+    });
+}
 
 if (require.main === module) {
   program.parse();
