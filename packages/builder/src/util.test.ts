561d3c1cffd1b46560db9503d185fbfc61d3ccae
Daniel Beal <git@danb.email>
2022-11-15 16:46:21 -0800

    lint

--- packages/builder/src/util.test.ts
+++ packages/builder/src/util.test.ts
@@ -1,74 +1,72 @@
 import { getMergedAbiFromContractPaths } from './util';
 
 import 'jest';
-import _ from 'lodash';
 import { ChainBuilderContext } from '.';
 import { JsonFragment } from '@ethersproject/abi';
 
 describe('util.ts', () => {
-
   const fakeTransferFragment: JsonFragment = {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
       },
       {
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
       },
       {
-        "internalType": "uint256",
-        "name": "tokenId",
-        "type": "uint256"
-      }
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
     ],
-    "name": "transferFrom",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'transferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
   };
 
   const fakeEventFragment: JsonFragment = {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
       },
       {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "tokenId",
-        "type": "uint256"
-      }
+        indexed: true,
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
     ],
-    "name": "Transfer",
-    "type": "event"
+    name: 'Transfer',
+    type: 'event',
   };
 
   const fakeReadFragment: JsonFragment = {
-    "inputs": [],
-    "name": "isInitialized",
-    "outputs": [
+    inputs: [],
+    name: 'isInitialized',
+    outputs: [
       {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function',
   };
 
   const fakeCtx: ChainBuilderContext = {
@@ -79,10 +77,7 @@ describe('util.ts', () => {
         sourceName: '',
         deployTxnHash: '',
         deployedOn: '',
-        abi: [
-          fakeEventFragment,
-          fakeReadFragment,
-        ]
+        abi: [fakeEventFragment, fakeReadFragment],
       },
 
       AnotherFake: {
@@ -91,9 +86,7 @@ describe('util.ts', () => {
         sourceName: '',
         deployTxnHash: '',
         deployedOn: '',
-        abi: [
-          fakeTransferFragment
-        ]
+        abi: [fakeTransferFragment],
       },
     },
     imports: {
@@ -105,19 +98,16 @@ describe('util.ts', () => {
             sourceName: '',
             deployTxnHash: '',
             deployedOn: '',
-            abi: [
-              fakeTransferFragment,
-              fakeReadFragment
-            ]
-          }
-        }
-      }
+            abi: [fakeTransferFragment, fakeReadFragment],
+          },
+        },
+      },
     },
     settings: {},
     txns: {},
     chainId: 0,
     timestamp: '0',
-    package: {}
+    package: {},
   };
 
   describe('getMergedAbiFromContractPaths()', () => {
