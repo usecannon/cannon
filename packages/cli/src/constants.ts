77ce4b9358e86b14d92511a8ffce3748c51f374f
Matías <mjlescano@protonmail.com>
2023-01-26 23:09:44 +0100

    update registry default address

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -4,5 +4,5 @@ import os from 'node:os';
 export const DEFAULT_CANNON_DIRECTORY = path.join(os.homedir(), '.local', 'share', 'cannon');
 export const DEFAULT_REGISTRY_IPFS_ENDPOINT = 'https://usecannon.infura-ipfs.io';
 export const DEFAULT_REGISTRY_ENDPOINT = 'https://cloudflare-eth.com/v1/mainnet';
-export const DEFAULT_REGISTRY_ADDRESS = '0xA98BE35415Dd28458DA4c1C034056766cbcaf642';
+export const DEFAULT_REGISTRY_ADDRESS = '0x8E5C7EFC9636A6A0408A46BB7F617094B81e5dba';
 export const CLI_SETTINGS_STORE = 'settings.json';
