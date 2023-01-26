8cd5a0abb91e722be726cbfabdb723f2c4f69f60
Matías Lescano <mjlescano@users.noreply.github.com>
2023-01-26 20:41:48 +0100

    Updates to the registry (#98)
    * use registry misc contracts as a dependency
    
    * fix tests
    
    * update package-lock

--- packages/registry/contracts/CannonRegistry.sol
+++ packages/registry/contracts/CannonRegistry.sol
@@ -1,11 +1,9 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity 0.8.17;
 
+import {SetUtil} from "@synthetixio/core-contracts/contracts/utils/SetUtil.sol";
+import {OwnedUpgradable} from "./OwnedUpgradable.sol";
 import {Storage} from "./Storage.sol";
-
-import "./misc/SetUtil.sol";
-
-import "./OwnedUpgradable.sol";
 
 contract CannonRegistry is Storage, OwnedUpgradable {
   using SetUtil for SetUtil.Bytes32Set;
@@ -13,7 +11,7 @@ contract CannonRegistry is Storage, OwnedUpgradable {
   error Unauthorized();
   error InvalidUrl(string url);
   error InvalidName(bytes32 name);
-  error TooManyTags();
+  error InvalidTags();
   error PackageNotFound();
 
   event PackagePublish(
@@ -68,8 +66,8 @@ contract CannonRegistry is Storage, OwnedUpgradable {
     string memory _packageVersionUrl,
     string memory _packageMetaUrl
   ) external {
-    if (_packageTags.length > 5) {
-      revert TooManyTags();
+    if (_packageTags.length == 0 || _packageTags.length > 5) {
+      revert InvalidTags();
     }
 
     if (bytes(_packageVersionUrl).length == 0) {
--- packages/registry/contracts/OwnedUpgradable.sol
+++ packages/registry/contracts/OwnedUpgradable.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {Ownable} from "./misc/Ownable.sol";
-import {UUPSImplementation} from "./misc/UUPSImplementation.sol";
+import {Ownable} from "@synthetixio/core-contracts/contracts/ownership/Ownable.sol";
+import {UUPSImplementation} from "@synthetixio/core-contracts/contracts/proxy/UUPSImplementation.sol";
 
 contract OwnedUpgradable is Ownable, UUPSImplementation {
   function upgradeTo(address _newImplementation) public override onlyOwner {
