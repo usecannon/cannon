2c9b6854954417252ff69b9d89079c2d86f644e1
Matías <mjlescano@protonmail.com>
2023-02-01 19:03:55 +0100

    add multicall function to registry

--- packages/registry/contracts/CannonRegistry.sol
+++ packages/registry/contracts/CannonRegistry.sol
@@ -195,4 +195,28 @@ contract CannonRegistry is Storage, EfficientStorage, OwnedUpgradable {
 
     return k;
   }
+
+  // @title Function that enables calling multiple methods of the system in a single transaction.
+  // @dev Implementation adapted from https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol
+  function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
+    results = new bytes[](data.length);
+
+    for (uint256 i = 0; i < data.length; i++) {
+      (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+      if (!success) {
+        // Next 6 lines from https://ethereum.stackexchange.com/a/83577
+        // solhint-disable-next-line reason-string
+        if (result.length < 68) revert();
+
+        assembly {
+          result := add(result, 0x04)
+        }
+
+        revert(abi.decode(result, (string)));
+      }
+
+      results[i] = result;
+    }
+  }
 }
