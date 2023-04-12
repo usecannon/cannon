c0385ad78b9ce2b1e7b1daf50c06dae3799bfebd
Matías <mjlescano@protonmail.com>
2023-04-12 22:50:59 +0200

    load plugins on hardhat build

--- packages/hardhat-cannon/src/tasks/build.ts
+++ packages/hardhat-cannon/src/tasks/build.ts
@@ -132,7 +132,6 @@ task(TASK_BUILD, 'Assemble a defined chain and save it to to a state which can b
       wipe,
       persist: !dryRun && hre.network.name !== 'hardhat',
       overrideResolver: dryRun ? createDryRunRegistry(resolveCliSettings()) : undefined,
-      plugins: false,
       publicSourceCode: hre.config.cannon.publicSourceCode,
     } as const;
 
