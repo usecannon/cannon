d45af01dbb3d5ed7ffefa2606dec86b189ac0756
Daniel Beal <git@danb.email>
2023-04-25 15:45:26 -0700

    support for recursive verify
    now all provisioned packages will be eligible for publish upon call to
    `cannon verify`.
    
    This is achieved with a new function `forPackageTree` which basically
    executes an action for a given `DeploymentInfo` object recursively, one
    at a time.
    
    We could also use this function to do all kinds of other things to do
    with subpackages.

--- packages/builder/src/package.ts
+++ packages/builder/src/package.ts
@@ -14,23 +14,66 @@ export type CopyPackageOpts = {
   recursive?: boolean;
 };
 
-export async function copyPackage(opts: CopyPackageOpts) {
-  const calls = await copyIpfs(opts);
+/**
+ * Iterate Depth-First-Search over the given DeploymentInfo and its dependencies, and execute the given `action` function. Postfix execution (aka, `action` is only executed after dependants are completed).
+ * Each package executes one at a time. No paralellization.
+ * @param loader The loader to use for downloading sub-packages
+ * @param deployInfo The head node of the tree, which will be executed on `action` last
+ * @param action The action to execute
+ * @param onlyProvisioned Skip over sub-packages which are not provisioned within the parent
+ */
+export async function forPackageTree<T>(loader: CannonLoader, deployInfo: DeploymentInfo, action: (deployInfo: DeploymentInfo) => Promise<T>, onlyProvisioned = true): Promise<T[]> {
+  const results: T[] = [];
 
-  return opts.toLoader.resolver.publishMany(calls);
+  for (const stepState of Object.entries(deployInfo.state || {})) {
+    for (const importArtifact of Object.entries((stepState[1] as StepState).artifacts.imports || {})) {
+      const nestedDeployInfo = await loader.readMisc(importArtifact[1].url);
+      await forPackageTree(loader, nestedDeployInfo, action, onlyProvisioned);
+
+    }
+  }
+
+  results.push(await action(deployInfo));
+
+  return results;
 }
 
-export async function copyIpfs({
+export async function copyPackage({
   packageRef,
   tags,
   variant,
   fromLoader,
   toLoader,
   recursive,
-}: CopyPackageOpts): Promise<{ packagesNames: string[]; variant: string; url: string; metaUrl: string }[]> {
-  debug(`copy package ${packageRef} (${fromLoader.getLabel()} -> ${toLoader.getLabel()})`);
+}: CopyPackageOpts) {
 
-  const registrationCalls: { packagesNames: string[]; variant: string; url: string; metaUrl: string }[] = [];
+  // this internal function will copy one package's ipfs records and return a publish call, without recursing
+  const copyIpfs = async (deployInfo: DeploymentInfo) => {
+    const miscUrl = await toLoader.putMisc(await fromLoader.readMisc(deployData!.miscUrl));
+
+    const metaUrl = await fromLoader.resolver.getMetaUrl(packageRef, variant);
+    let newMetaUrl = metaUrl;
+
+    if (metaUrl) {
+      newMetaUrl = await toLoader.putMisc(await fromLoader.readMisc(metaUrl));
+    }
+    const url = await toLoader.putDeploy(deployData!);
+
+    if (!url || /*url !== toPublishUrl || */ newMetaUrl !== metaUrl || miscUrl !== deployInfo.miscUrl) {
+      throw new Error('re-deployed urls do not match up');
+    }
+
+    const def = new ChainDefinition(deployInfo.def);
+
+    const preCtx = await createInitialContext(def, deployInfo.meta, 0, deployInfo.options);
+
+    return {
+      packagesNames: [def.getVersion(preCtx), ...tags].map((t) => `${def.getName(preCtx)}:${t}`),
+      variant,
+      url,
+      metaUrl: metaUrl || '',
+    };
+  }
 
   const chainId = parseInt(variant.split('-')[0]);
   const preset = variant.substring(variant.indexOf('-') + 1);
@@ -41,55 +84,12 @@ export async function copyIpfs({
     throw new Error('ipfs could not find deployment artifact. please double check your settings, and rebuild your package.');
   }
 
-  const def = new ChainDefinition(deployData.def);
-
   if (recursive) {
-    for (const stepState of Object.entries(deployData.state || {})) {
-      for (const importArtifact of Object.entries((stepState[1] as StepState).artifacts.imports || {})) {
-        // if there are any tags defined (even an empty array), then we assume that a publish should be done.
-        // otherwise, its a non-provisioned import and we shouldn't do anything
-        if (importArtifact[1].tags) {
-          // copy package nested
-          const nestedDeployInfo: DeploymentInfo = await fromLoader.readMisc(importArtifact[1].url);
-          const nestedDef = new ChainDefinition(nestedDeployInfo.def);
-          const preCtx = await createInitialContext(nestedDef, nestedDeployInfo.meta, 0, nestedDeployInfo.options);
-          registrationCalls.push(
-            ...(await copyIpfs({
-              packageRef: `${nestedDef.getName(preCtx)}:${nestedDef.getVersion(preCtx)}`,
-              variant: `${chainId}-${def.getConfig(stepState[0], preCtx).targetPreset}`,
-              tags: importArtifact[1].tags || [],
-              fromLoader,
-              toLoader,
-              recursive,
-            }))
-          );
-        }
-      }
-    }
+    const calls = await forPackageTree(fromLoader, deployData, copyIpfs);
+    return toLoader.resolver.publishMany(calls);
+  } else {
+    const call = await copyIpfs(deployData);
+
+    return toLoader.resolver.publish(call.packagesNames, call.variant, call.url, call.metaUrl);
   }
-
-  const miscUrl = await toLoader.putMisc(await fromLoader.readMisc(deployData!.miscUrl));
-
-  const metaUrl = await fromLoader.resolver.getMetaUrl(packageRef, variant);
-  let newMetaUrl = metaUrl;
-
-  if (metaUrl) {
-    newMetaUrl = await toLoader.putMisc(await fromLoader.readMisc(metaUrl));
-  }
-  const url = await toLoader.putDeploy(deployData!);
-
-  if (!url || /*url !== toPublishUrl || */ newMetaUrl !== metaUrl || miscUrl !== deployData.miscUrl) {
-    throw new Error('re-deployed urls do not match up');
-  }
-
-  const preCtx = await createInitialContext(def, deployData.meta, 0, deployData.options);
-
-  registrationCalls.push({
-    packagesNames: [def.getVersion(preCtx), ...tags].map((t) => `${def.getName(preCtx)}:${t}`),
-    variant,
-    url,
-    metaUrl: metaUrl || '',
-  });
-
-  return registrationCalls;
 }
