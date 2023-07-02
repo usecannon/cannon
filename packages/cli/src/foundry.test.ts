5feb8692b61a81c7bbfbc22c6ee9d0cdd5187c95
Amin Latifi <a.latifi.al@gmail.com>
2023-07-02 13:43:38 +0330

    Remove sinon from dependencies

--- packages/cli/src/foundry.test.ts
+++ packages/cli/src/foundry.test.ts
@@ -1,60 +1,57 @@
 import { getFoundryOpts, getFoundryArtifact, buildContracts } from './foundry';
 import * as helpers from './helpers';
-import sinon from 'sinon';
 
 import fs from 'fs-extra';
 
+jest.mock('./helpers');
+jest.mock('fs-extra');
+
 describe('getFoundryOpts', () => {
   it('should return the correct options', async () => {
-    const execPromiseStub = sinon
-      .stub(helpers, 'execPromise')
-      .resolves('{"src":"src","test":"test","script":"script","out":"out"}');
+    jest.mocked(helpers.execPromise).mockResolvedValue('{"src":"src","test":"test","script":"script","out":"out"}');
 
     const opts = await getFoundryOpts();
     expect(opts).toEqual({ src: 'src', test: 'test', script: 'script', out: 'out' });
-
-    execPromiseStub.restore();
   });
 });
 
 describe('buildContracts', () => {
   it('should call forge build', async () => {
-    const execPromiseStub = sinon.stub(helpers, 'execPromise').resolves();
+    jest.mock('./helpers');
 
     await buildContracts();
 
-    expect(execPromiseStub.calledWith('forge build')).toBe(true);
-
-    execPromiseStub.restore();
+    expect(helpers.execPromise).toHaveBeenCalledWith('forge build');
   });
 });
 
 describe('getFoundryArtifact', () => {
   it('should return the correct artifact', async () => {
-    const execPromiseStub = sinon.stub(helpers, 'execPromise');
-
-    execPromiseStub.onCall(0).resolves('{"src":"src","test":"test","script":"script","out":"out"}');
-
-    execPromiseStub.onCall(1).resolves(
-      JSON.stringify({
-        compiler: { version: '0.8.1' },
-        sources: { 'test.sol': {} },
-        settings: { optimizer: {}, remappings: {}, outputSelection: { '*': { '*': ['*'] } } },
-      })
-    );
-
-    const readFileStub = sinon.stub(fs, 'readFile').resolves(
-      Buffer.from(
+    jest
+      .mocked(helpers.execPromise)
+      .mockResolvedValueOnce('{"src":"src","test":"test","script":"script","out":"out"}')
+      .mockResolvedValueOnce(
         JSON.stringify({
-          ast: { absolutePath: 'test.sol' },
-          abi: [],
-          bytecode: { object: '0x1234', linkReferences: {} },
-          deployedBytecode: { object: '0x1234' },
+          compiler: { version: '0.8.1' },
+          sources: { 'test.sol': {} },
+          settings: { optimizer: {}, remappings: {}, outputSelection: { '*': { '*': ['*'] } } },
         })
+      );
+
+    jest.mocked(fs.readFile).mockReturnValue(
+      Promise.resolve(
+        Buffer.from(
+          JSON.stringify({
+            ast: { absolutePath: 'test.sol' },
+            abi: [],
+            bytecode: { object: '0x1234', linkReferences: {} },
+            deployedBytecode: { object: '0x1234' },
+          })
+        )
       )
     );
 
-    const readFileSyncStub = sinon.stub(fs, 'readFileSync').returns('test contract');
+    jest.mocked(fs.readFileSync).mockReturnValue('test contract');
 
     const artifact = await getFoundryArtifact('Test');
 
@@ -66,9 +63,5 @@ describe('getFoundryArtifact', () => {
     expect(artifact).toHaveProperty('deployedBytecode');
     expect(artifact).toHaveProperty('linkReferences');
     expect(artifact).toHaveProperty('source');
-
-    execPromiseStub.restore();
-    readFileStub.restore();
-    readFileSyncStub.restore();
   });
 });
