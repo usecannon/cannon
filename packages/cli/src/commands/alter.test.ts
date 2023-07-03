441e3a3a04628ba011eb8cb9e65848868fee8dfb
Amin Latifi <a.latifi.al@gmail.com>
2023-07-03 18:08:13 +0330

    Added test for set-contract-address of alter

--- packages/cli/src/commands/alter.test.ts
+++ packages/cli/src/commands/alter.test.ts
@@ -1,6 +1,5 @@
 import { ethers } from 'ethers';
 import { alter } from './alter';
-import { runRpc } from '../rpc';
 import { createDefaultReadRegistry } from '../registry';
 import { CannonStorage, DeploymentInfo, FallbackRegistry, IPFSLoader } from '@usecannon/builder';
 import { getMainLoader, LocalLoader } from '../loader';
@@ -28,41 +27,43 @@ jest.mock('../loader');
 jest.mock('../rpc');
 
 describe('alter', () => {
+  const packageName = 'package:1.2.3';
+  const chainId = 123;
+  const preset = 'your-preset';
+  const runtimeOverrides = {};
+  const metaUrl = 'meta-url';
+  const url = 'file:/usecannon.com/url';
+  const newUrl = 'file:/usecannon.com/new-url';
+  let testPkgData: DeploymentInfo;
+  let localLoader: LocalLoader;
+  let ipfsLoader: IPFSLoader;
+  let mockedFallBackRegistry: FallbackRegistry;
+
   beforeEach(() => {
+    // Reset mocks
     jest.clearAllMocks();
-  });
 
-  test('should perform alteration for set-url command', async () => {
-    // Set up test data and variables
-    const packageName = 'package:1.2.3';
-    const chainId = 123;
-    const preset = 'your-preset';
-    const command = 'set-url';
-    const url = 'file:/usecannon.com/url';
-    const newUrl = 'file:/usecannon.com/new-url';
-    const metaUrl = 'meta-url';
-    const targets = [url];
-    const runtimeOverrides = {};
-
-    const localLoader = new LocalLoader('path');
-    const ipfsLoader = new IPFSLoader('ipfs');
-    const mockedFallBackRegistry = new FallbackRegistry([]);
-
-    jest.spyOn(mockedFallBackRegistry, 'publish').mockResolvedValue([]);
-
-    jest.mocked(createDefaultReadRegistry).mockResolvedValue(Promise.resolve(mockedFallBackRegistry));
-    jest.mocked(getMainLoader).mockReturnValueOnce({
-      file: localLoader,
-      ipfs: ipfsLoader,
-    });
-    jest.spyOn(mockedFallBackRegistry, 'getMetaUrl').mockResolvedValue(metaUrl);
-    const testPkgData: DeploymentInfo = {
+    localLoader = new LocalLoader('path');
+    ipfsLoader = new IPFSLoader('ipfs');
+    mockedFallBackRegistry = new FallbackRegistry([]);
+    testPkgData = {
       def: { name: 'package', version: '1.2.3', provision: { dummyStep: { tags: ['tag3', 'tag4'] } } } as any,
       state: {
         'provision.dummyStep': {
           hash: '',
           version: 1,
-          artifacts: {},
+          artifacts: {
+            contracts: {
+              TestContract: {
+                address: '0x1111111111111111111111111111111111111111',
+                abi: [],
+                deployTxnHash: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+                contractName: 'TestContract',
+                sourceName: 'TestContract.sol',
+                deployedOn: '',
+              },
+            },
+          },
         },
       },
       status: 'complete',
@@ -70,6 +71,21 @@ describe('alter', () => {
       meta: {},
       options: {},
     };
+    jest.spyOn(mockedFallBackRegistry, 'publish').mockResolvedValue([]);
+    jest.spyOn(mockedFallBackRegistry, 'getMetaUrl').mockResolvedValue(metaUrl);
+    jest.spyOn(CannonStorage.prototype, 'readDeploy').mockResolvedValue(testPkgData);
+    jest.spyOn(CannonStorage.prototype, 'putDeploy').mockResolvedValue(newUrl);
+    jest.mocked(createDefaultReadRegistry).mockResolvedValue(Promise.resolve(mockedFallBackRegistry));
+    jest.mocked(getMainLoader).mockReturnValueOnce({
+      file: localLoader,
+      ipfs: ipfsLoader,
+    });
+  });
+
+  test('should perform alteration for set-url command', async () => {
+    // Set up test data and variables
+    const command = 'set-url';
+    const targets = [url];
 
     const newTestPkgData: DeploymentInfo = _.assign(testPkgData, {
       miscUrl: 'file:/usecannon.com/new-misc',
@@ -90,10 +106,7 @@ describe('alter', () => {
       }
     });
 
-    jest.spyOn(CannonStorage.prototype, 'readDeploy').mockResolvedValue(testPkgData);
-    jest.spyOn(CannonStorage.prototype, 'putDeploy').mockResolvedValue(newUrl);
     jest.spyOn(localLoader, 'read').mockImplementation(mockReadDeploy);
-    jest.spyOn(ipfsLoader, 'put').mockResolvedValue(newUrl);
 
     // Call the 'alter' function with the necessary arguments
     await alter(packageName, chainId, preset, testPkgData.meta, command, targets, runtimeOverrides);
@@ -106,31 +119,24 @@ describe('alter', () => {
       newUrl,
       metaUrl
     );
-
-    expect(runRpc as jest.Mock<any, any>).toHaveBeenCalledWith(expect.objectContaining({ port: expect.any(Number) }));
   });
 
-  // test('should perform alteration for set-contract-address command', async () => {
-  //   // Set up test data and variables
-  //   const packageRef = 'your-package-ref';
-  //   const chainId = 1;
-  //   const preset = 'your-preset';
-  //   const meta = {};
-  //   const command = 'set-contract-address';
-  //   const targets = ['target-contract-address'];
-  //   const runtimeOverrides = {};
-  //
-  //   // Call the 'alter' function with the necessary arguments
-  //   await alter(packageRef, chainId, preset, meta, command, targets, runtimeOverrides);
-  //
-  //   // Assert the expected behavior or outcome
-  //   // You can use jest assertions or expect statements here
-  //   expect(runRpc as jest.Mock<any, any>).toHaveBeenCalledWith(expect.objectContaining({ port: expect.any(Number) }));
-  //   expect(createDefaultReadRegistry as jest.Mock<any, any>).toHaveBeenCalled();
-  //   expect(getProvider as jest.Mock<any, any>).toHaveBeenCalled();
-  //   expect(getMainLoader as jest.Mock<any, any>).toHaveBeenCalled();
-  //   expect(_.assign as jest.Mock<any, any>).toHaveBeenCalled();
-  // });
-  //
-  // Add more tests for the other commands like 'mark-complete' and 'mark-incomplete'
+  test('should perform alteration for set-contract-address command', async () => {
+    // Set up test data and variables
+    const command = 'set-contract-address';
+    const targets = ['TestContract', '0x2222222222222222222222222222222222222222'];
+
+    // Call the 'alter' function with the necessary arguments
+    await alter(packageName, chainId, preset, testPkgData.meta, command, targets, runtimeOverrides);
+
+    expect(CannonStorage.prototype.readDeploy as jest.Mock<any, any>).toHaveBeenCalledWith(packageName, preset, chainId);
+    expect(CannonStorage.prototype.putDeploy as jest.Mock<any, any>).toHaveBeenCalledWith(testPkgData);
+    expect(testPkgData.state['provision.dummyStep'].artifacts.contracts!.TestContract.address).toEqual(targets[1]);
+    expect(mockedFallBackRegistry.publish as jest.Mock<any, any>).toHaveBeenCalledWith(
+      [packageName],
+      `${chainId}-${preset}`,
+      newUrl,
+      metaUrl
+    );
+  });
 });
