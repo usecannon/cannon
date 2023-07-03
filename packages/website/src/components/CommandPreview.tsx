e07521c44993134c74e4b2d52fedcb9b04465af0
Cherik <Pourcheriki@gmail.com>
2023-07-03 16:01:34 +0330

    add CommandPreview

---
+++ packages/website/src/components/CommandPreview.tsx
@@ -0,0 +1,43 @@
+import { Box, Text, useClipboard } from "@chakra-ui/react";
+import { FC } from "react";
+import { Copy } from "react-feather";
+
+interface ICommandPreviewProps {
+  command: string;
+  className?: string;
+}
+
+export const CommandPreview: FC<ICommandPreviewProps> = ({
+  command,
+  className,
+}) => {
+  const { hasCopied, onCopy } = useClipboard(command);
+  const index = command.indexOf(" ");
+  const firstPart = command.substring(0, index);
+  const secondPart = command.substring(index, command.length);
+  return (
+    <Box
+      backgroundColor="black"
+      py={1}
+      px={3}
+      position="relative"
+      className={className}
+    >
+      <Text fontFamily="var(--font-mono)">
+        <Text as="span" color="#61afef">
+          {firstPart}
+        </Text>
+        &nbsp;{secondPart}
+      </Text>
+      <Box
+        position="absolute"
+        top="10px"
+        right="10px"
+        cursor="pointer"
+        onClick={onCopy}
+      >
+        {hasCopied ? <Text fontSize="xs">Copied</Text> : <Copy size={16} />}
+      </Box>
+    </Box>
+  );
+};
