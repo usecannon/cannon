b3c8cb2c074eecf56f19ef99a1fe41e267395bcc
Cherik <Pourcheriki@gmail.com>
2023-07-04 11:25:17 +0330

    move to home page

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,9 +1,5 @@
 'use client';
-import { HowItWorks } from '@/Features/HomePage/HowItWorks';
-import { Intro } from '@/Features/HomePage/Intro';
-import { QuickStart } from '@/Features/HomePage/QuickStart';
-import { UseCases } from '@/Features/HomePage/UseCases';
-import { Container, SimpleGrid, Box } from '@chakra-ui/react';
+import HomePage from '@/Features/HomePage/HomePage';
 import Head from 'next/head';
 
 export default function Home() {
@@ -13,18 +9,7 @@ export default function Home() {
         <title>Cannon</title>
         <meta name="title" content="Cannon" />
       </Head>
-      <Intro />
-      <Container maxW="container.lg">
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={32} py={16}>
-          <Box>
-            <HowItWorks />
-          </Box>
-          <Box>
-            <QuickStart />
-          </Box>
-        </SimpleGrid>
-        <UseCases />
-      </Container>
+      <HomePage />
     </>
   );
 }
