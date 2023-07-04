96406c001ae273903e4f178d27610064be39427b
Cherik <Pourcheriki@gmail.com>
2023-07-04 11:20:25 +0330

    add home page title

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -4,10 +4,15 @@ import { Intro } from '@/Features/HomePage/Intro';
 import { QuickStart } from '@/Features/HomePage/QuickStart';
 import { UseCases } from '@/Features/HomePage/UseCases';
 import { Container, SimpleGrid, Box } from '@chakra-ui/react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Cannon</title>
+        <meta name="title" content="Cannon" />
+      </Head>
       <Intro />
       <Container maxW="container.lg">
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={32} py={16}>
