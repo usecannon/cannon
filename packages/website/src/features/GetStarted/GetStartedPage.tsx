8e3e069332c0e56a73709e63aabf3583cc8b6b85
Cherik <Pourcheriki@gmail.com>
2023-07-04 15:41:30 +0330

    use RunPackage

--- packages/website/src/features/GetStarted/GetStartedPage.tsx
+++ packages/website/src/features/GetStarted/GetStartedPage.tsx
@@ -1,13 +1,15 @@
 'use client';
 import { Container, Heading } from '@chakra-ui/react';
 import React from 'react';
+import { RunPackage } from './RunPackage';
 
 export const GetStartedPage = () => {
   return (
     <Container maxW="container.lg">
-      <Heading as="h2" mb={4} textAlign="left">
+      <Heading as="h2" mb={8} textAlign="left">
         Get Started with Cannon
       </Heading>
+      <RunPackage />
     </Container>
   );
 };
