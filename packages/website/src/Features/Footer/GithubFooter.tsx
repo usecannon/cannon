4e4059c5b986de007ab671ce853ceedc47c9d657
Cherik <Pourcheriki@gmail.com>
2023-07-04 10:13:15 +0330

    update GithubFooter styles

--- packages/website/src/Features/Footer/GithubFooter.tsx
+++ packages/website/src/Features/Footer/GithubFooter.tsx
@@ -1,9 +1,17 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
-import { GitHub } from "react-feather";
+import { Flex, Link } from '@chakra-ui/react';
+import { GitHub } from 'react-feather';
 
 export const GithubFooter = () => {
   return (
-    <Flex alignItems="center" justifyContent="center" gap={2} py={2} px={4}>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      gap={2}
+      py={2}
+      px={4}
+      backgroundColor="black"
+      mt={16}
+    >
       <GitHub size={16} />
       <Link
         href="https://github.com/usecannon/cannon"
