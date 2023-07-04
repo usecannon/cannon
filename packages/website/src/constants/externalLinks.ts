a61453effb8e2f2f75b45eab6c4a4029c4a2badb
Cherik <Pourcheriki@gmail.com>
2023-07-04 15:40:52 +0330

    add GITHUB_ANVIL

--- packages/website/src/constants/externalLinks.ts
+++ packages/website/src/constants/externalLinks.ts
@@ -3,6 +3,7 @@ const github = 'https://github.com/';
 export const externalLinks = {
   GITHUB_CANNON: github + 'usecannon/cannon',
   GITHUB_FOUNDRY: github + 'foundry-rs/foundry',
+  GITHUB_ANVIL: github + 'foundry-rs/foundry/tree/master/anvil',
   TERRAFORM: 'https://www.terraform.io/',
   HARD_HAT: 'https://hardhat.org/',
 };
