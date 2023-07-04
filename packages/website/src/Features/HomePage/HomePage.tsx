a6f787797efd50d291c871aae483aec6248ae09a
Cherik <Pourcheriki@gmail.com>
2023-07-04 11:51:12 +0330

    add home page styles

--- packages/website/src/Features/HomePage/HomePage.tsx
+++ packages/website/src/Features/HomePage/HomePage.tsx
@@ -4,22 +4,25 @@ import { Intro } from '@/Features/HomePage/Intro';
 import { QuickStart } from '@/Features/HomePage/QuickStart';
 import { UseCases } from '@/Features/HomePage/UseCases';
 import { Container, SimpleGrid, Box } from '@chakra-ui/react';
+import styles from './homePage.module.scss';
 
 export default function HomePage() {
   return (
     <>
       <Intro />
-      <Container maxW="container.lg">
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={32} py={16}>
-          <Box>
-            <HowItWorks />
-          </Box>
-          <Box>
-            <QuickStart />
-          </Box>
-        </SimpleGrid>
-        <UseCases />
-      </Container>
+      <Box className={styles.wrapper}>
+        <Container maxW="container.lg">
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={32} py={16} mt={16}>
+            <Box>
+              <HowItWorks />
+            </Box>
+            <Box>
+              <QuickStart />
+            </Box>
+          </SimpleGrid>
+          <UseCases />
+        </Container>
+      </Box>
     </>
   );
 }
