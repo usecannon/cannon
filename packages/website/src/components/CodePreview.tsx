6eebfe2181c8bc4626e213da8781ba3d7e71bd6e
Cherik <Pourcheriki@gmail.com>
2023-07-10 18:27:47 +0330

    use chakra components

--- packages/website/src/components/CodePreview.tsx
+++ packages/website/src/components/CodePreview.tsx
@@ -3,7 +3,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import style from './codePreview.module.scss';
 interface ICodePreviewProps {
-  code: string;
+  code: string | string[];
   language: string;
 }
 export const CodePreview: FC<ICodePreviewProps> = ({ code, language }) => {
--- packages/website/src/features/Docs/DocsPage.tsx
+++ packages/website/src/features/Docs/DocsPage.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { IContentList } from '@/app/docs/page';
-import { Container, Heading } from '@chakra-ui/react';
+import { CodePreview } from '@/components/CodePreview';
+import { Text, Code, Container, Heading } from '@chakra-ui/react';
 import { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
 
@@ -14,10 +15,30 @@ export const DocsPage: FC<IDocsPageProps> = ({ content }) => {
     <Container maxW="container.lg">
       <ReactMarkdown
         components={{
-          // Map `h1` (`# heading`) to use `h2`s.
-          h1: Heading,
+          p: ({ ...props }) => <Text {...props} mt={4} />,
+          h1: ({ ...props }) => <Heading as="h1" {...props} mb={4} />,
+          h2: ({ ...props }) => (
+            <Heading as="h2" {...props} mb={4} pt={8} fontSize={24} />
+          ),
+          h3: ({ ...props }) => (
+            <Heading as="h3" {...props} mb={4} pt={8} fontSize={20} />
+          ),
+          code: ({ inline, className, ...props }) => {
+            const lang = className?.replace('language-', '') || 'javascript';
+            console.log('props.children', props.children);
+            return inline ? (
+              <Code colorScheme="blackAlpha" variant="solid" {...props} />
+            ) : (
+              <CodePreview
+                // @ts-ignore
+                code={props.children || ''}
+                language={lang}
+                {...props}
+              />
+            );
+          },
           // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-          em: ({ ...props }) => <i style={{ color: 'red' }} {...props} />,
+          // em: ({ ...props }) => <i style={{ color: 'red' }} {...props} />,
         }}
       >
         {content}
