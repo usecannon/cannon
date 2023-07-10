49318e52ca6e057ec981a3747cc2f7fedbfd9a42
Cherik <Pourcheriki@gmail.com>
2023-07-10 17:08:47 +0330

    init docs page server side component

--- packages/website/src/app/docs/page.tsx
+++ packages/website/src/app/docs/page.tsx
@@ -1,6 +1,34 @@
-'use client';
-import { Heading } from '@chakra-ui/react';
+import { DocsPage } from '@/features/Docs/DocsPage';
+import fs from 'fs';
+import path from 'path';
+
+// const titles = ['Test': ['Test Child1 ', 'Test Child2'] }];
+
+export interface IContentList {
+  [key: string]: string[];
+}
 
 export default function Docs() {
-  return <Heading>Docs</Heading>;
+  const markdownWithMetadata = fs
+    .readFileSync(path.join('content', 'docs-technical' + '.md'))
+    .toString();
+  const lines = markdownWithMetadata.split('\n');
+  const titles = lines.filter((line) => line.startsWith('##'));
+  const list: IContentList = {};
+  let key = '';
+  for (let i = 0; i < titles.length; i++) {
+    const title = titles[i];
+    if (title.startsWith('## ')) {
+      key = title.substring(3);
+      if (!list[key]) {
+        list[key] = [];
+      }
+    }
+    if (title.startsWith('### ')) {
+      const child = title.substring(4);
+      list[key].push(child);
+    }
+  }
+
+  return <DocsPage content={markdownWithMetadata} list={list} />;
 }
