21b888406966f7f9e676406b79dbe4169df11ca3
Cherik <Pourcheriki@gmail.com>
2023-07-13 17:38:28 +0330

    fetch all data

--- packages/website/src/app/docs/page.tsx
+++ packages/website/src/app/docs/page.tsx
@@ -1,34 +1,30 @@
 import { DocsPage } from '@/features/Docs/DocsPage';
+import { exportListFromMarkdown } from '@/helpers/markdown';
 import fs from 'fs';
 import path from 'path';
 
-// const titles = ['Test': ['Test Child1 ', 'Test Child2'] }];
-
-export interface IContentList {
-  [key: string]: string[];
-}
-
 export default function Docs() {
-  const markdownWithMetadata = fs
+  const overviewMarkdown = fs
+    .readFileSync(path.join('content', 'docs-overview' + '.md'))
+    .toString();
+
+  const technicalMarkdown = fs
     .readFileSync(path.join('content', 'docs-technical' + '.md'))
     .toString();
-  const lines = markdownWithMetadata.split('\n');
-  const titles = lines.filter((line) => line.startsWith('##'));
-  const list: IContentList = {};
-  let key = '';
-  for (let i = 0; i < titles.length; i++) {
-    const title = titles[i];
-    if (title.startsWith('## ')) {
-      key = title.substring(3);
-      if (!list[key]) {
-        list[key] = [];
-      }
-    }
-    if (title.startsWith('### ')) {
-      const child = title.substring(4);
-      list[key].push(child);
-    }
-  }
 
-  return <DocsPage content={markdownWithMetadata} list={list} />;
+  const overviewList = exportListFromMarkdown(overviewMarkdown);
+  const technicalList = exportListFromMarkdown(technicalMarkdown);
+
+  const contents = {
+    overview: {
+      list: overviewList,
+      md: overviewMarkdown,
+    },
+    technical: {
+      list: technicalList,
+      md: technicalMarkdown,
+    },
+  };
+
+  return <DocsPage contents={contents} />;
 }
