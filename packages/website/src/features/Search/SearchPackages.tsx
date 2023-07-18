e5147f37da56f7769d1176f55d29528c7f542a02
Amin Latifi <a.latifi.al@gmail.com>
2023-07-18 17:36:33 +0330

    Added graphql type support to the repo

--- packages/website/src/features/Search/SearchPackages.tsx
+++ packages/website/src/features/Search/SearchPackages.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Grid, GridItem, Input } from '@chakra-ui/react';
 import { useQuery } from '@apollo/client';
 import { GET_PACKAGES } from '@/graphql/queries';
+import { Package } from '@/types/graphql/graphql';
 
 export const SearchPackages = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -9,9 +10,15 @@ export const SearchPackages = () => {
     variables: { query: searchTerm, skip: 0, first: 10 },
   });
 
+  const [packages, setPackages] = useState<Package[]>([]);
+
   const handleSearch = (e: any) => {
     setSearchTerm(e.target.value);
   };
+
+  useEffect(() => {
+    console.log('Definition:', GET_PACKAGES.definitions);
+  }, []);
 
   useEffect(() => {
     if (loading) {
@@ -23,6 +30,7 @@ export const SearchPackages = () => {
     if (error) {
       console.log('error:', error);
     }
+    setPackages(data?.packages || []);
   }, [loading, error, data]);
 
   return (
