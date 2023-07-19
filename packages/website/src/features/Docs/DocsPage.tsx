f5cb734602a4ecaf78c6b6817f90e1373c6c6be0
Cherik <Pourcheriki@gmail.com>
2023-07-19 17:03:29 +0330

    init rendering md

--- packages/website/src/features/Docs/DocsPage.tsx
+++ packages/website/src/features/Docs/DocsPage.tsx
@@ -1,9 +1,17 @@
 'use client';
 import { CodePreview } from '@/components/CodePreview';
-import { Text, Code, Container, Heading } from '@chakra-ui/react';
+import {
+  Text,
+  Code,
+  Container,
+  Heading,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
 import { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { IContentList } from '@/helpers/markdown';
+import { headingToId } from '@/helpers/markdown';
 
 interface IDocsPageProps {
   contents: {
@@ -18,36 +26,53 @@ export const DocsPage: FC<IDocsPageProps> = ({ contents }) => {
   const content = contents.overview.md;
   return (
     <Container maxW="container.lg">
-      <ReactMarkdown
-        components={{
-          p: ({ ...props }) => <Text {...props} mt={4} />,
-          h1: ({ ...props }) => <Heading as="h1" {...props} mb={4} />,
-          h2: ({ ...props }) => (
-            <Heading as="h2" {...props} mb={4} pt={8} fontSize={24} />
-          ),
-          h3: ({ ...props }) => (
-            <Heading as="h3" {...props} mb={4} pt={8} fontSize={20} />
-          ),
-          code: ({ inline, className, ...props }) => {
-            const lang = className?.replace('language-', '') || 'javascript';
-            console.log('props.children', props.children);
-            return inline ? (
-              <Code colorScheme="blackAlpha" variant="solid" {...props} />
-            ) : (
-              <CodePreview
-                // @ts-ignore
-                code={props.children || ''}
-                language={lang}
-                {...props}
-              />
-            );
-          },
-          // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-          // em: ({ ...props }) => <i style={{ color: 'red' }} {...props} />,
-        }}
-      >
-        {content}
-      </ReactMarkdown>
+      <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+        <GridItem colSpan={3}>Menu</GridItem>
+        <GridItem colSpan={9}>
+          <ReactMarkdown
+            components={{
+              p: ({ ...props }) => <Text {...props} mt={4} />,
+              h1: ({ ...props }) => <Heading as="h1" {...props} mb={4} />,
+              h2: ({ ...props }) => (
+                <Heading
+                  as="h2"
+                  {...props}
+                  mb={4}
+                  pt={8}
+                  fontSize={24}
+                  id={headingToId(props)}
+                />
+              ),
+              h3: ({ ...props }) => (
+                <Heading
+                  as="h3"
+                  {...props}
+                  mb={4}
+                  pt={8}
+                  fontSize={20}
+                  id={headingToId(props)}
+                />
+              ),
+              code: ({ inline, className, ...props }) => {
+                const lang =
+                  className?.replace('language-', '') || 'javascript';
+                console.log('props.children', props.children);
+                return inline ? (
+                  <Code colorScheme="blackAlpha" variant="solid" {...props} />
+                ) : (
+                  <CodePreview
+                    code={(props.children[0] as string) || ''}
+                    language={lang}
+                    {...props}
+                  />
+                );
+              },
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+        </GridItem>
+      </Grid>
     </Container>
   );
 };
