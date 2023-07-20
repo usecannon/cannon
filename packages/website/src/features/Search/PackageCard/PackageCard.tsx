25dfb8780615dcec0998092143bcaf605ea918ed
Amin Latifi <a.latifi.al@gmail.com>
2023-07-20 16:27:43 +0330

    Added link to package card

--- packages/website/src/features/Search/PackageCard/PackageCard.tsx
+++ packages/website/src/features/Search/PackageCard/PackageCard.tsx
@@ -1,7 +1,6 @@
 import PackageNetworks from '@/features/Search/PackageNetworks';
 import { GetPackagesQuery } from '@/types/graphql/graphql';
-import { Box, Flex, Heading } from '@chakra-ui/react';
-import { redirect } from 'next/navigation';
+import { Box, Flex, Heading, LinkOverlay } from '@chakra-ui/react';
 import { FC } from 'react';
 import PublishInfo from './PublishInfo';
 
@@ -11,29 +10,30 @@ interface IPackageCardProps {
 
 export const PackageCard: FC<IPackageCardProps> = ({ pkg }) => {
   return (
-    <Box
-      key={pkg.name}
-      onClick={() => redirect(`/packages/${pkg.name}`)}
-      p="6"
-      bg="blue.975"
-      display="block"
-      borderWidth="1px"
-      borderStyle="solid"
-      borderColor="rgba(255,255,255,0.15)"
-      mb="6"
-      borderRadius="4px"
-      _hover={{ bg: 'blue.950', borderColor: 'rgba(255,255,255,0.25)' }}
-      transition="0.12s"
-    >
-      <Flex alignItems="['', 'center']" direction={['column', 'row']} mb="2">
-        <Heading as="h4" size="md" mb={[1, 0]}>
-          {pkg.name}
-        </Heading>
-        <Box ml={[0, 'auto']}>
-          <PublishInfo p={pkg} />
-        </Box>
-      </Flex>
-      <PackageNetworks p={pkg} />
-    </Box>
+    <LinkOverlay href={`/packages/${pkg.name}`}>
+      <Box
+        key={pkg.name}
+        p="6"
+        bg="blue.975"
+        display="block"
+        borderWidth="1px"
+        borderStyle="solid"
+        borderColor="rgba(255,255,255,0.15)"
+        mb="6"
+        borderRadius="4px"
+        _hover={{ bg: 'blue.950', borderColor: 'rgba(255,255,255,0.25)' }}
+        transition="0.12s"
+      >
+        <Flex alignItems="['', 'center']" direction={['column', 'row']} mb="2">
+          <Heading as="h4" size="md" mb={[1, 0]}>
+            {pkg.name}
+          </Heading>
+          <Box ml={[0, 'auto']}>
+            <PublishInfo p={pkg} />
+          </Box>
+        </Flex>
+        <PackageNetworks p={pkg} />
+      </Box>
+    </LinkOverlay>
   );
 };
