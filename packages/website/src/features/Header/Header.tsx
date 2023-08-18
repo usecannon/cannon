0d96957e4f8225d4affecae9c831bbefca899f79
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-08-18 09:49:28 -0400

    mobile header fix

--- packages/website/src/features/Header/Header.tsx
+++ packages/website/src/features/Header/Header.tsx
@@ -9,14 +9,13 @@ import { NavLink } from '@/components/NavLink';
 export const Header = () => {
   return (
     <Box bg="black" borderBottom="1px solid" borderColor="gray.700">
-      <Flex align="center" mx="auto" px={4} flexWrap="wrap">
+      <Flex align="center" mx="auto" pt={[4, 4, 0]} px={4} flexWrap="wrap">
         <Link
           href={links.HOMEPAGE}
           color="white"
           as={NextLink}
           textDecoration="none"
           _hover={{ textDecoration: 'none' }}
-          mb={[4, 4, 0]}
           display={['block', 'block', 'inline']}
         >
           <Flex gap={1} alignItems="center">
@@ -31,11 +30,13 @@ export const Header = () => {
         </Link>
         <Spacer display={['none', 'none', 'block']} />
         <Box display={['block', 'block', 'none']} w="100%" />
-        <Flex alignItems="center" flexWrap="wrap">
-          <NavLink href={links.EXPLORE}>Explore</NavLink>
-          <NavLink href={'https://deploy.usecannon.com'}>Deploy</NavLink>
-          <NavLink href={links.LEARN}>Learn</NavLink>
-        </Flex>
+        <Box width={['100%', '100%', 'auto']} mt={[1, 1, 0]}>
+          <Flex alignItems="center" flexWrap="wrap" justifyContent="center">
+            <NavLink href={links.EXPLORE}>Explore</NavLink>
+            <NavLink href={'https://deploy.usecannon.com'}>Deploy</NavLink>
+            <NavLink href={links.LEARN}>Learn</NavLink>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
