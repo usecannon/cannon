188c900d9de209cba4f80261315f19aea1d65051
FuzzB0t <christopherpagnusatt@gmail.com>
2023-08-25 12:55:48 -0600

    fix(cli): remove anvil from inspect function

--- packages/cli/src/commands/inspect.ts
+++ packages/cli/src/commands/inspect.ts
@@ -5,11 +5,9 @@ import { createDefaultReadRegistry } from '../registry';
 import { resolveCliSettings } from '../settings';
 import fs from 'fs-extra';
 import path from 'path';
-import { setupAnvil } from '../helpers';
 import { getMainLoader } from '../loader';
 
 export async function inspect(packageRef: string, chainId: number, preset: string, json: boolean, writeDeployments: string) {
-  await setupAnvil();
   const { name, version } = parsePackageRef(packageRef);
 
   const resolver = await createDefaultReadRegistry(resolveCliSettings());
@@ -22,6 +20,13 @@ export async function inspect(packageRef: string, chainId: number, preset: strin
     throw new Error(
       `deployment not found: ${`${name}:${version}`}. please make sure it exists for the variant ${chainId}-${preset}.`
     );
+  }
+
+  if (!chainId) {
+    console.warn(yellow(
+      "The deployment data for the latest local version of this package (which runs with 'cannon PACKAGE_NAME') was exported. \
+      Specify the --chain-id parameter to retrieve the addresses/ABIs for other deployments."
+    ))
   }
 
   const deployData = await loader[deployUrl.split(':')[0] as 'ipfs' | 'file'].read(deployUrl);
