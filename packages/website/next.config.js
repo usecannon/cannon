ce7e32fd03649dbcaec87704d817a3c6023ffdd8
0xDmitry <sign-69@mail.ru>
2023-09-12 13:31:46 +0300

    feat(website): include sourcemaps on prod

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding', 'solc');
