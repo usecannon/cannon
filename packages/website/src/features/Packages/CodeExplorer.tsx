817518c71f22233cf2a895a5ddc0576df0a98662
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-09-15 12:02:43 -0500

    code tab

--- packages/website/src/features/Packages/CodeExplorer.tsx
+++ packages/website/src/features/Packages/CodeExplorer.tsx
@@ -1,14 +1,40 @@
 import { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import pako from 'pako';
-import { Box, Container } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Code,
+  Container,
+  Flex,
+  Heading,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Tooltip,
+  useDisclosure,
+} from '@chakra-ui/react';
 import 'prismjs';
 import 'prismjs/components/prism-toml';
 import { CodePreview } from '@/components/CodePreview';
-import { isEmpty } from 'lodash';
 import { IpfsUrl } from './IpfsUrl';
-import { CustomSpinner } from '@/components/CustomSpinner';
 import { useQuery } from '@tanstack/react-query';
+import { DownloadIcon, InfoIcon, ViewIcon } from '@chakra-ui/icons';
+
+const handleDownload = (content: JSON) => {
+  const blob = new Blob([JSON.stringify(content, null, 2)], {
+    type: 'application/json',
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'deployments.json';
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+};
 
 export const CodeExplorer: FC<{
   variant: any;
@@ -85,30 +111,142 @@ export const CodeExplorer: FC<{
     enabled: !!miscUrl,
   });
 
+  const {
+    isOpen: isCodeDataModalOpen,
+    onOpen: openCodeDataModal,
+    onClose: closeCodeDataModal,
+  } = useDisclosure();
+
+  const {
+    isOpen: isMetadataModalOpen,
+    onOpen: openMetadataModal,
+    onClose: closeMetadataModal,
+  } = useDisclosure();
+
   return (
     <Container maxW="container.lg">
-      {variant.meta_url && (
-        <Box mb={8}>
-          <IpfsUrl title="Metadata" url={variant.meta_url} />
-          {loading ? (
-            <Box py="20" textAlign="center">
-              <CustomSpinner mx="auto" />
-            </Box>
-          ) : !isEmpty(metadata) ? (
+      {miscData?.data &&
+        Object.entries(JSON.parse(miscData?.data).artifacts).map(
+          ([artifactKey, artifactValue]) => {
+            return (
+              <Box mb={8}>
+                <Flex flexDirection={['column', 'column', 'row']} mb="2">
+                  <Heading size="md" mb="2">
+                    {artifactKey}
+                  </Heading>
+
+                  <Button
+                    variant="outline"
+                    colorScheme="white"
+                    mb={2}
+                    size="xs"
+                    color="gray.300"
+                    borderColor="gray.500"
+                    _hover={{ bg: 'gray.700' }}
+                    leftIcon={<DownloadIcon />}
+                    onClick={() => {
+                      handleDownload(artifactValue?.abi);
+                    }}
+                    ml={[0, 0, 'auto']}
+                  >
+                    Download ABI
+                  </Button>
+                </Flex>
+                {Object.entries(
+                  JSON.parse(artifactValue?.source?.input).sources
+                ).map(([sourceKey, sourceValue]) => {
+                  return (
+                    <Box mb={6}>
+                      <Code fontSize="lg" mb="4">
+                        {sourceKey}
+                      </Code>
+                      <CodePreview
+                        code={sourceValue?.content}
+                        language="solidity"
+                      />
+                    </Box>
+                  );
+                })}
+              </Box>
+            );
+          }
+        )}
+
+      <Box mb={6}>
+        <Heading size="md" mb={4}>
+          Code Data{' '}
+          <Tooltip
+            label="This is the source of the data displayed above."
+            placement="right"
+            hasArrow
+          >
+            <InfoIcon color="gray.400" boxSize={4} mt={-1} ml={1} />
+          </Tooltip>
+        </Heading>
+        <Button
+          variant="outline"
+          colorScheme="white"
+          onClick={openCodeDataModal}
+          mb={3}
+          leftIcon={<ViewIcon />}
+        >
+          View Code Data
+        </Button>
+        {variant?.deploy_url && <IpfsUrl url={variant.deploy_url} />}
+
+        <Modal
+          isOpen={isCodeDataModalOpen}
+          onClose={closeCodeDataModal}
+          size="6xl"
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
+            <CodePreview
+              code={JSON.stringify(miscData.data, null, 2)}
+              language="json"
+            />
+          </ModalContent>
+        </Modal>
+      </Box>
+
+      <Box mb={6}>
+        <Heading size="md" mb={4}>
+          Metadata{' '}
+          <Tooltip
+            label="This includes data like the Cannonfile used to build the package."
+            placement="right"
+            hasArrow
+          >
+            <InfoIcon color="gray.400" boxSize={4} mt={-1} ml={1} />
+          </Tooltip>
+        </Heading>
+        <Button
+          variant="outline"
+          colorScheme="white"
+          onClick={openMetadataModal}
+          mb={3}
+          leftIcon={<ViewIcon />}
+        >
+          View Metadata
+        </Button>
+        {variant?.meta_url && <IpfsUrl url={variant.meta_url} />}
+
+        <Modal
+          isOpen={isMetadataModalOpen}
+          onClose={closeMetadataModal}
+          size="6xl"
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
             <CodePreview
               code={JSON.stringify(metadata, null, 2)}
               language="json"
             />
-          ) : (
-            <Box textAlign="center" py="20" opacity="0.5">
-              Unable to retrieve metadata
-            </Box>
-          )}
-        </Box>
-      )}
-
-      <IpfsUrl title="Miscellaneous Data" url={miscUrl ?? ''} />
-      {miscData.data && <CodePreview code={miscData.data} language="json" />}
+          </ModalContent>
+        </Modal>
+      </Box>
     </Container>
   );
 };
