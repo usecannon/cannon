cc661234ed01dd44397559bc2282eac9f7c7f5dc
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-09-16 20:00:28 -0500

    init

--- packages/website/src/app/learn/guides/debug/page.tsx
+++ packages/website/src/app/learn/guides/debug/page.tsx
@@ -2,7 +2,7 @@ import { DebugPage } from '@/features/Debug/DebugPage';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Cannon | Debug',
+  title: 'Cannon | Debug a Protocol',
 };
 
 export default function Home() {
