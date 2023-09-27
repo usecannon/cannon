60b68fd64481a99b52d68fcf6d234b8624dd7260
FuzzB0t <christopherpagnusatt@gmail.com>
2023-09-27 14:07:59 -0600

    chore: fix lint issues

--- packages/cli/src/commands/decode.ts
+++ packages/cli/src/commands/decode.ts
@@ -21,7 +21,7 @@ export async function decode({
   const { preset, basePackageRef } = new PackageReference(packageRef);
 
   if (!data[0].startsWith('0x')) {
-    data[0] = data[0].replace(/^/, '0x')
+    data[0] = data[0].replace(/^/, '0x');
   }
 
   if (presetArg && preset) {
