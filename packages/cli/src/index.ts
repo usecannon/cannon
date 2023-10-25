c93504618750a65c8b8af206f2ab9800586e5ca8
Dmitry <61683409+0xDmitry@users.noreply.github.com>
2023-10-25 22:47:51 +0300

    refactor(cli): add commands config (#533)
    Co-authored-by: Matías Lescano <mjlescano@users.noreply.github.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -103,7 +103,48 @@ function applyCommandsConfig(command: Command, config: any) {
   return command;
 }
 
+function applyCommandsConfig(command: Command, config: any) {
+  if (config.description) {
+    command.description(config.description);
+  }
+  if (config.usage) {
+    command.usage(config.usage);
+  }
+  if (config.arguments) {
+    config.arguments.map((argument: any) => {
+      if (argument.flags === '<packageNames...>') {
+        command.argument(argument.flags, argument.description, parsePackagesArguments, argument.defaultValue);
+      } else if (command.name() === 'interact' && argument.flags === '<packageName>') {
+        command.argument(argument.flags, argument.description, parsePackageArguments, argument.defaultValue);
+      } else {
+        command.argument(argument.flags, argument.description, argument.defaultValue);
+      }
+    });
+  }
+  if (config.anvilOptions) {
+    config.anvilOptions.map((option: any) => {
+      option.required
+        ? command.requiredOption(option.flags, option.description, option.defaultValue)
+        : command.option(option.flags, option.description, option.defaultValue);
+    });
+  }
+  if (config.options) {
+    config.options.map((option: any) => {
+      option.required
+        ? command.requiredOption(option.flags, option.description, option.defaultValue)
+        : command.option(option.flags, option.description, option.defaultValue);
+    });
+  }
+  return command;
+}
+
 function configureRun(program: Command) {
+  return applyCommandsConfig(program, commandsConfig.run).action(async function (
+    packages: PackageSpecification[],
+    options,
+    program
+  ) {
+    const { run } = await import('./commands/run');
   return applyCommandsConfig(program, commandsConfig.run).action(async function (
     packages: PackageSpecification[],
     options,
@@ -112,13 +153,26 @@ function configureRun(program: Command) {
     const { run } = await import('./commands/run');
 
     options.port = Number.parseInt(options.port) || 8545;
+    options.port = Number.parseInt(options.port) || 8545;
 
+    let node: CannonRpcNode;
+    if (options.chainId) {
+      const settings = resolveCliSettings(options);
     let node: CannonRpcNode;
     if (options.chainId) {
       const settings = resolveCliSettings(options);
 
       const { provider } = await resolveWriteProvider(settings, Number.parseInt(options.chainId));
+      const { provider } = await resolveWriteProvider(settings, Number.parseInt(options.chainId));
 
+      if (options.providerUrl) {
+        const providerChainId = (await provider.getNetwork()).chainId;
+        if (providerChainId != options.chainId) {
+          throw new Error(
+            `Supplied providerUrl's blockchain chainId ${providerChainId} does not match with chainId you provided ${options.chainId}`
+          );
+        }
+      }
       if (options.providerUrl) {
         const providerChainId = (await provider.getNetwork()).chainId;
         if (providerChainId != options.chainId) {
@@ -134,7 +188,19 @@ function configureRun(program: Command) {
     } else {
       node = await runRpc(pickAnvilOptions(options));
     }
+      node = await runRpc(pickAnvilOptions(options), {
+        forkProvider: provider.passThroughProvider as ethers.providers.JsonRpcProvider,
+      });
+    } else {
+      node = await runRpc(pickAnvilOptions(options));
+    }
 
+    await run(packages, {
+      ...options,
+      node,
+      helpInformation: program.helpInformation(),
+    });
+  });
     await run(packages, {
       ...options,
       node,
@@ -313,7 +379,23 @@ applyCommandsConfig(program.command('verify'), commandsConfig.verify).action(asy
   const { verify } = await import('./commands/verify');
   await verify(packageName, options.apiKey, options.preset, options.chainId);
 });
+applyCommandsConfig(program.command('verify'), commandsConfig.verify).action(async function (packageName, options) {
+  const { verify } = await import('./commands/verify');
+  await verify(packageName, options.apiKey, options.preset, options.chainId);
+});
 
+applyCommandsConfig(program.command('alter'), commandsConfig.alter).action(async function (
+  packageName,
+  command,
+  options,
+  flags
+) {
+  const { alter } = await import('./commands/alter');
+  // note: for command below, pkgInfo is empty because forge currently supplies no package.json or anything similar
+  await alter(packageName, flags.chainId, flags.preset, {}, command, options, {
+    getArtifact: getFoundryArtifact,
+  });
+});
 applyCommandsConfig(program.command('alter'), commandsConfig.alter).action(async function (
   packageName,
   command,
@@ -398,11 +480,27 @@ applyCommandsConfig(program.command('publish'), commandsConfig.publish).action(a
 
   const cliSettings = resolveCliSettings(options);
   const p = await resolveRegistryProvider(cliSettings);
+  const cliSettings = resolveCliSettings(options);
+  const p = await resolveRegistryProvider(cliSettings);
 
   const overrides: ethers.PayableOverrides = {};
+  const overrides: ethers.Overrides = {};
+
+  if (!options.chainId) {
+    throw new Error(
+      'Please provide a chainId using the format: --chain-id <number>. For example, 13370 is the chainId for a local build.'
+    );
+  }
 
   if (options.maxFeePerGas) {
     overrides.maxFeePerGas = ethers.utils.parseUnits(options.maxFeePerGas, 'gwei');
+  }
+  if (options.maxFeePerGas) {
+    overrides.maxFeePerGas = ethers.utils.parseUnits(options.maxFeePerGas, 'gwei');
+  }
+
+  if (options.maxPriorityFeePerGas) {
+    overrides.maxPriorityFeePerGas = ethers.utils.parseUnits(options.maxPriorityFeePerGas, 'gwei');
   }
 
   if (options.gasLimit) {
@@ -421,6 +519,18 @@ applyCommandsConfig(program.command('publish'), commandsConfig.publish).action(a
     }\n - Gas Limit: ${overrides.gasLimit ? overrides.gasLimit : 'default'}\n` +
       " - To alter these settings use the parameters '--max-fee-per-gas', '--max-priority-fee-per-gas', '--gas-limit'.\n"
   );
+  if (options.gasLimit) {
+    overrides.gasLimit = options.gasLimit;
+  }
+  console.log(
+    `Settings:\nMax Fee Per Gas: ${
+      overrides.maxFeePerGas ? overrides.maxFeePerGas.toString() : 'default'
+    }\nMax Priority Fee Per Gas: ${
+      overrides.maxPriorityFeePerGas ? overrides.maxPriorityFeePerGas.toString() : 'default'
+    }\nGas Limit: ${
+      overrides.gasLimit ? overrides.gasLimit : 'default'
+    }\nTo alter these settings use the parameters '--max-fee-per-gas', '--max-priority-fee-per-gas', '--gas-limit'.`
+  );
 
   await publish({
     packageRef,
@@ -434,6 +544,16 @@ applyCommandsConfig(program.command('publish'), commandsConfig.publish).action(a
     overrides,
   });
 });
+  await publish({
+    packageRef,
+    signer: p.signers[0],
+    tags: options.tags.split(','),
+    chainId: options.chainId ? Number.parseInt(options.chainId) : undefined,
+    presetArg: options.preset ? (options.preset as string) : undefined,
+    quiet: options.quiet,
+    overrides,
+  });
+});
 
 applyCommandsConfig(program.command('inspect'), commandsConfig.inspect).action(async function (packageName, options) {
   const { inspect } = await import('./commands/inspect');
@@ -444,15 +564,34 @@ applyCommandsConfig(program.command('inspect'), commandsConfig.inspect).action(a
 applyCommandsConfig(program.command('prune'), commandsConfig.prune).action(async function (options) {
   const { prune } = await import('./commands/prune');
   resolveCliSettings(options);
+applyCommandsConfig(program.command('inspect'), commandsConfig.inspect).action(async function (packageName, options) {
+  const { inspect } = await import('./commands/inspect');
+  resolveCliSettings(options);
+  await inspect(packageName, options.chainId, options.preset, options.json, options.writeDeployments);
+});
 
+applyCommandsConfig(program.command('prune'), commandsConfig.prune).action(async function (options) {
+  const { prune } = await import('./commands/prune');
+  resolveCliSettings(options);
+
+  const registry = await createDefaultReadRegistry(resolveCliSettings());
   const registry = await createDefaultReadRegistry(resolveCliSettings());
 
   const loader = getMainLoader(resolveCliSettings());
+  const loader = getMainLoader(resolveCliSettings());
 
+  const storage = new CannonStorage(registry, loader);
   const storage = new CannonStorage(registry, loader);
 
   console.log('Scanning for storage artifacts to prune (this may take some time)...');
+  console.log('Scanning for storage artifacts to prune (this may take some time)...');
 
+  const [pruneUrls, pruneStats] = await prune(
+    storage,
+    options.filterPackage?.split(',') || '',
+    options.filterVariant?.split(',') || '',
+    options.keepAge
+  );
   const [pruneUrls, pruneStats] = await prune(
     storage,
     options.filterPackage?.split(',') || '',
@@ -465,7 +604,15 @@ applyCommandsConfig(program.command('prune'), commandsConfig.prune).action(async
     console.log(`Matched with Registry: ${pruneStats.matchedFromRegistry}`);
     console.log(`Not Expired: ${pruneStats.notExpired}`);
     console.log(`Not Cannon Package: ${pruneStats.notCannonPackage}`);
+  if (pruneUrls.length) {
+    console.log(bold(`Found ${pruneUrls.length} storage artifacts to prune.`));
+    console.log(`Matched with Registry: ${pruneStats.matchedFromRegistry}`);
+    console.log(`Not Expired: ${pruneStats.notExpired}`);
+    console.log(`Not Cannon Package: ${pruneStats.notCannonPackage}`);
 
+    if (options.dryRun) {
+      process.exit(0);
+    }
     if (options.dryRun) {
       process.exit(0);
     }
@@ -477,7 +624,19 @@ applyCommandsConfig(program.command('prune'), commandsConfig.prune).action(async
         message: 'Delete these artifacts?',
         initial: true,
       });
+    if (!options.yes) {
+      const verification = await prompts({
+        type: 'confirm',
+        name: 'confirmation',
+        message: 'Delete these artifacts?',
+        initial: true,
+      });
 
+      if (!verification.confirmation) {
+        console.log('Cancelled');
+        process.exit(1);
+      }
+    }
       if (!verification.confirmation) {
         console.log('Cancelled');
         process.exit(1);
@@ -492,13 +651,28 @@ applyCommandsConfig(program.command('prune'), commandsConfig.prune).action(async
         console.error(`Failed to delete ${url}: ${err.message}`);
       }
     }
+    for (const url of pruneUrls) {
+      console.log(`delete ${url}`);
+      try {
+        await storage.deleteBlob(url);
+      } catch (err: any) {
+        console.error(`Failed to delete ${url}: ${err.message}`);
+      }
+    }
 
     console.log('Done!');
   } else {
     console.log(bold('Nothing to prune.'));
   }
 });
+    console.log('Done!');
+  } else {
+    console.log(bold('Nothing to prune.'));
+  }
+});
 
+applyCommandsConfig(program.command('trace'), commandsConfig.trace).action(async function (packageName, data, options) {
+  const { trace } = await import('./commands/trace');
 applyCommandsConfig(program.command('trace'), commandsConfig.trace).action(async function (packageName, data, options) {
   const { trace } = await import('./commands/trace');
 
@@ -515,6 +689,20 @@ applyCommandsConfig(program.command('trace'), commandsConfig.trace).action(async
     json: options.json,
   });
 });
+  await trace({
+    packageName,
+    data,
+    chainId: options.chainId,
+    preset: options.preset,
+    providerUrl: options.providerUrl,
+    from: options.from,
+    to: options.to,
+    value: options.value,
+    block: options.blockNumber,
+    json: options.json,
+  });
+});
+
 
 applyCommandsConfig(program.command('decode'), commandsConfig.decode).action(async function (packageRef, data, options) {
   const { decode } = await import('./commands/decode');
@@ -528,20 +716,34 @@ applyCommandsConfig(program.command('decode'), commandsConfig.decode).action(asy
   });
 });
 
+
+applyCommandsConfig(program.command('test'), commandsConfig.test).action(async function (cannonfile, forgeOpts, opts) {
+  opts.port = 8545;
+  const [node, outputs] = await doBuild(cannonfile, [], opts);
 applyCommandsConfig(program.command('test'), commandsConfig.test).action(async function (cannonfile, forgeOpts, opts) {
   opts.port = 8545;
   const [node, outputs] = await doBuild(cannonfile, [], opts);
 
   // basically we need to write deployments here
   await writeModuleDeployments(path.join(process.cwd(), 'deployments/test'), '', outputs);
+  // basically we need to write deployments here
+  await writeModuleDeployments(path.join(process.cwd(), 'deployments/test'), '', outputs);
 
+  // after the build is done we can run the forge tests for the user
+  const forgeCmd = spawn('forge', ['test', '--fork-url', 'http://localhost:8545', ...forgeOpts]);
   // after the build is done we can run the forge tests for the user
   const forgeCmd = spawn('forge', ['test', '--fork-url', 'http://localhost:8545', ...forgeOpts]);
 
   forgeCmd.stdout.on('data', (data: Buffer) => {
     process.stdout.write(data);
   });
+  forgeCmd.stdout.on('data', (data: Buffer) => {
+    process.stdout.write(data);
+  });
 
+  forgeCmd.stderr.on('data', (data: Buffer) => {
+    process.stderr.write(data);
+  });
   forgeCmd.stderr.on('data', (data: Buffer) => {
     process.stderr.write(data);
   });
@@ -554,14 +756,27 @@ applyCommandsConfig(program.command('test'), commandsConfig.test).action(async f
     });
   });
 });
+  await new Promise((resolve) => {
+    forgeCmd.on('close', (code: number) => {
+      console.log(`forge exited with code ${code}`);
+      node?.kill();
+      resolve({});
+    });
+  });
+});
 
+applyCommandsConfig(program.command('interact'), commandsConfig.interact).action(async function (packageDefinition, opts) {
+  const cliSettings = resolveCliSettings(opts);
 applyCommandsConfig(program.command('interact'), commandsConfig.interact).action(async function (packageDefinition, opts) {
   const cliSettings = resolveCliSettings(opts);
 
   const p = await resolveWriteProvider(cliSettings, opts.chainId);
+  const p = await resolveWriteProvider(cliSettings, opts.chainId);
 
   const networkInfo = await p.provider.getNetwork();
+  const networkInfo = await p.provider.getNetwork();
 
+  const resolver = await createDefaultReadRegistry(cliSettings);
   const resolver = await createDefaultReadRegistry(cliSettings);
 
   const runtime = new ChainBuilderRuntime(
@@ -583,7 +798,27 @@ applyCommandsConfig(program.command('interact'), commandsConfig.interact).action
     resolver,
     getMainLoader(cliSettings)
   );
+  const runtime = new ChainBuilderRuntime(
+    {
+      provider: p.provider,
+      chainId: networkInfo.chainId,
+      async getSigner(addr: string) {
+        // on test network any user can be conjured
+        await p.provider.send('hardhat_impersonateAccount', [addr]);
+        await p.provider.send('hardhat_setBalance', [addr, `0x${(1e22).toString(16)}`]);
+        return p.provider.getSigner(addr);
+      },
+      snapshots: false,
+      allowPartialDeploy: false,
+      gasPrice: opts.gasPrice,
+      gasFee: opts.maxGasFee,
+      priorityGasFee: opts.maxPriorityFee,
+    },
+    resolver,
+    getMainLoader(cliSettings)
+  );
 
+  const selectedPreset = packageDefinition.preset || opts.preset || 'main';
   const selectedPreset = packageDefinition.preset || opts.preset || 'main';
 
   const deployData = await runtime.readDeploy(
@@ -598,6 +833,7 @@ applyCommandsConfig(program.command('interact'), commandsConfig.interact).action
     );
   }
 
+  const outputs = await getOutputs(runtime, new ChainDefinition(deployData.def), deployData.state);
   const outputs = await getOutputs(runtime, new ChainDefinition(deployData.def), deployData.state);
 
   if (!outputs) {
@@ -617,7 +853,18 @@ applyCommandsConfig(program.command('interact'), commandsConfig.interact).action
     provider: p.provider,
   });
 });
+  await interact({
+    packages: [packageDefinition],
+    contracts,
+    signer: p.signers[0],
+    provider: p.provider,
+  });
+});
 
+applyCommandsConfig(program.command('setup'), commandsConfig.setup).action(async function () {
+  const { setup } = await import('./commands/setup');
+  await setup();
+});
 applyCommandsConfig(program.command('setup'), commandsConfig.setup).action(async function () {
   const { setup } = await import('./commands/setup');
   await setup();
@@ -628,9 +875,20 @@ applyCommandsConfig(program.command('clean'), commandsConfig.clean).action(async
   const executed = await clean(!noConfirm);
   if (executed) console.log('Complete!');
 });
+applyCommandsConfig(program.command('clean'), commandsConfig.clean).action(async function ({ noConfirm }) {
+  const { clean } = await import('./commands/clean');
+  const executed = await clean(!noConfirm);
+  if (executed) console.log('Complete!');
+});
 
 const pluginCmd = applyCommandsConfig(program.command('plugin'), commandsConfig.plugin);
+const pluginCmd = applyCommandsConfig(program.command('plugin'), commandsConfig.plugin);
 
+applyCommandsConfig(pluginCmd.command('list'), commandsConfig.plugin.commands.list).action(async function () {
+  console.log(green(bold('\n=============== Installed Plug-ins ===============')));
+  const installedPlugins = await listInstalledPlugins();
+  installedPlugins.forEach((plugin) => console.log(yellow(plugin)));
+});
 applyCommandsConfig(pluginCmd.command('list'), commandsConfig.plugin.commands.list).action(async function () {
   console.log(green(bold('\n=============== Installed Plug-ins ===============')));
   const installedPlugins = await listInstalledPlugins();
@@ -642,7 +900,17 @@ applyCommandsConfig(pluginCmd.command('add'), commandsConfig.plugin.commands.add
   await installPlugin(name);
   console.log('Complete!');
 });
+applyCommandsConfig(pluginCmd.command('add'), commandsConfig.plugin.commands.add).action(async function (name) {
+  console.log(`Installing plug-in ${name}...`);
+  await installPlugin(name);
+  console.log('Complete!');
+});
 
+applyCommandsConfig(pluginCmd.command('remove'), commandsConfig.plugin.commands.remove).action(async function (name) {
+  console.log(`Removing plugin ${name}...`);
+  await removePlugin(name);
+  console.log('Complete!');
+});
 applyCommandsConfig(pluginCmd.command('remove'), commandsConfig.plugin.commands.remove).action(async function (name) {
   console.log(`Removing plugin ${name}...`);
   await removePlugin(name);
