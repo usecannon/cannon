3cd9634dffb7930857b49dfb438ec38540a534c9
FuzzB0t <christopherpagnusatt@gmail.com>
2023-10-31 21:18:09 -0600

    fix tests

--- packages/hardhat-cannon/src/tasks/build.ts
+++ packages/hardhat-cannon/src/tasks/build.ts
@@ -64,10 +64,12 @@ task(TASK_BUILD, 'Assemble a defined chain and save it to to a state which can b
       const parsedSettings = parseSettings(settings);
 
       let anvilOpts;
-      if ((anvilOptions as string).endsWith('.json')) {
-        anvilOpts = JSON.parse(await fs.readFileSync(anvilOptions, 'utf8'));
-      } else {
-        anvilOpts = JSON.parse(anvilOptions);
+      if(anvilOptions) {
+        if ((anvilOptions as string).endsWith('.json')) {
+          anvilOpts = JSON.parse(await fs.readFileSync(anvilOptions, 'utf8'));
+        } else {
+          anvilOpts = JSON.parse(anvilOptions);
+        }
       }
       anvilOpts = pickAnvilOptions(anvilOpts);
 
