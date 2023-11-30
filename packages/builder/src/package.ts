8f8948116ba6a4ae37b1fc7e1077fde4e95ef1df
FuzzB0t <christopherpagnusatt@gmail.com>
2023-11-30 09:43:24 -0600

    fixing lint issues

--- packages/builder/src/package.ts
+++ packages/builder/src/package.ts
@@ -176,7 +176,7 @@ export async function publishPackage({
   includeProvisioned = false,
 }: CopyPackageOpts) {
   debug(`copy package ${packageRef} (${fromStorage.registry.getLabel()} -> ${toStorage.registry.getLabel()})`);
-  
+
   // TODO: packageRef in this case can be a package name or an IPFS hash (@ipfs://Qm...) for the pin command, however, this functionality should have
   // it's own function to handle the pinning of IPFS urls.
   const fullPackageRef = !packageRef.startsWith('@') ? new PackageReference(packageRef).fullPackageRef : packageRef;
