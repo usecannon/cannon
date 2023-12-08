a6a720959b914705799558871b3a868532a3e90a
FuzzB0t <christopherpagnusatt@gmail.com>
2023-12-07 17:12:24 -0800

    fixing lint issues

--- packages/builder/src/package.ts
+++ packages/builder/src/package.ts
@@ -199,7 +199,7 @@ export async function publishPackage({
   // TODO: packageRef in this case can be a package name or an IPFS hash (@ipfs://Qm...) for the pin command, however, this functionality should have
   // it's own function to handle the pinning of IPFS urls.
   const packageReference = PackageReference.isValid(packageRef) ? new PackageReference(packageRef) : null;
-  
+
   const presetRef = packageReference ? packageReference.preset : 'main';
   const fullPackageRef = packageReference ? packageReference.fullPackageRef : packageRef;
 
