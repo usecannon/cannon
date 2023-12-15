d800d47038746747b1443c4481bbcbbb32b5a9cf
Daniel Beal <git@dbeal.dev>
2023-12-16 07:57:57 +0900

    simple start for e2e testing with bash on cli
    definitely buggy

---
+++ packages/cli/scripts/run-e2e.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+BOLD='\033[0;37;1m' # Bold white
+NC='\033[0m' # No Color
+
+export CANNON="node bin/cannon.js"
+
+function clean_env () {
+  if [ -e $HOME/.local/share/cannon ]; then
+    mv $HOME/.local/share/cannon $HOME/.local/share/cannon-untest
+  fi
+}
+
+for f in e2e/*; do
+  clean_env
+  echo -e "${BOLD}TEST $f${NC}"
+  bash $f 
+  rm -rf $HOME/.local/share/cannon
+done
