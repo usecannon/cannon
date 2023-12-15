993211b852fbbcb0109daea0b1430bc24631c4ab
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-12-15 08:32:50 -0500

    fix(website): quick explore page refresh (#688)

--- packages/website/src/features/Search/PackageCard/PackageCardExpandable.tsx
+++ packages/website/src/features/Search/PackageCard/PackageCardExpandable.tsx
@@ -3,18 +3,14 @@ import PackageTable from './PackageTable';
 import { Package } from '@/types/graphql/graphql';
 import {
   Box,
-  Collapse,
   Flex,
   Heading,
   Link,
   useDisclosure,
-  Text,
   Button,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { FC } from 'react';
-import PublishInfo from '@/features/Search/PackageCard/PublishInfo';
-import ChainNav from './ChainNav';
 
 interface IPackageCardProps {
   pkg: Package;
@@ -26,14 +22,6 @@ export const PackageCardExpandable: FC<IPackageCardProps> = ({
   maxHeight,
 }) => {
   const { isOpen, onToggle } = useDisclosure();
-
-  const latestUpdate = pkg.variants.reduce((max, variant) => {
-    return max > variant.last_updated ? max : variant.last_updated;
-  }, pkg.variants[0]?.last_updated);
-
-  const latestMainVariants = pkg.variants.filter(
-    (variant) => variant.tag.name === 'latest' && variant.preset === 'main'
-  );
 
   return (
     <Box
@@ -49,44 +37,31 @@ export const PackageCardExpandable: FC<IPackageCardProps> = ({
     >
       <Flex
         bg="gray.800"
-        p={2}
         flexDirection={['column', 'column', 'row']}
         alignItems={['flex-start', 'flex-start', 'center']}
+        px={3}
+        py={2}
       >
-        <Box py={2} px={[1, 1, 3]}>
-          <Heading display="inline-block" as="h4" size="md" mb={1.5}>
+        <Box>
+          <Heading display="inline-block" as="h4" size="sm">
             {pkg.name}
           </Heading>
           <Link
             as={NextLink}
             href={'/packages/' + pkg.name}
             display="inline-block"
-            ml={2}
+            ml={1.5}
             transform="translateY(-2px)"
           >
-            <LinkIcon />
+            <LinkIcon boxSize="3" />
           </Link>
-          <PublishInfo p={{ last_updated: latestUpdate }} />
         </Box>
-        <Box p={1} ml={[0, 0, 'auto']}>
-          {latestMainVariants.length ? (
-            <Flex
-              mb="2"
-              justifyContent={['flex-start', 'flex-start', 'flex-end']}
-            >
-              <ChainNav packageName={pkg.name} variants={latestMainVariants} />
-            </Flex>
-          ) : null}
+        <Box ml={[0, 0, 'auto']}>
           <Flex justifyContent={['flex-start', 'flex-start', 'flex-end']}>
-            <Text fontSize="sm" color="gray.300" display="inline" mr={2}>
-              {pkg.variants.length} variant
-              {pkg.variants.length !== 1 ? 's' : ''}
-            </Text>
             <Button
               size="xs"
               variant="outline"
               colorScheme="black"
-              width="56px"
               fontWeight={500}
               textTransform="uppercase"
               letterSpacing="1px"
@@ -100,33 +75,14 @@ export const PackageCardExpandable: FC<IPackageCardProps> = ({
               }}
               onClick={onToggle}
             >
-              {isOpen ? 'Hide' : 'Show'}
+              {isOpen ? 'Show Less' : 'Show More'}
             </Button>
           </Flex>
         </Box>
       </Flex>
-      <Collapse in={isOpen} animateOpacity>
-        <Box
-          borderTop="1px solid"
-          borderColor="gray.600"
-          position="relative"
-          _after={{
-            content: '""',
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0,
-            height: '4px',
-            background:
-              'linear-gradient(to top, rgba(0, 0, 0, 0.5), transparent)',
-            pointerEvents: 'none',
-          }}
-        >
-          <Box verticalAlign="middle" overflow="auto" maxHeight={maxHeight}>
-            <PackageTable pkg={pkg} />
-          </Box>
-        </Box>
-      </Collapse>
+      <Box verticalAlign="middle" overflow="auto" maxHeight={maxHeight}>
+        <PackageTable latestOnly={!isOpen} pkg={pkg} />
+      </Box>
     </Box>
   );
 };
