5dd5e2025dd341550eda87312258a91376fc1a2b
Daniel Beal <git@dbeal.dev>
2024-01-10 21:01:22 +0900

    add missing file

---
+++ packages/cli/src/terminal.ts
@@ -0,0 +1,163 @@
+import readline from 'readline';
+
+// low-level terminal interactions
+// Copied and adapted from: https://github.com/npkgz/cli-progress/blob/e2347a35a6c0922cfb0a077cf5ed21696fba46da/lib/terminal.js
+export class Terminal {
+  stream: typeof process.stdin;
+  linewrap: boolean;
+  dy: number;
+  constructor(outputStream: typeof process.stdin) {
+    this.stream = outputStream;
+
+    // default: line wrapping enabled
+    this.linewrap = true;
+
+    // current, relative y position
+    this.dy = 0;
+  }
+
+  // save cursor position + settings
+  cursorSave() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // save position
+    this.stream.write('\x1B7');
+  }
+
+  // restore last cursor position + settings
+  cursorRestore() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // restore cursor
+    this.stream.write('\x1B8');
+  }
+
+  // show/hide cursor
+  cursor(enabled: boolean) {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    if (enabled) {
+      this.stream.write('\x1B[?25h');
+    } else {
+      this.stream.write('\x1B[?25l');
+    }
+  }
+
+  // change cursor positionn
+  cursorTo(x = 0, y = 0) {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // move cursor absolute
+    readline.cursorTo(this.stream, x, y);
+  }
+
+  // change relative cursor position
+  cursorRelative(dx = 0, dy = 0) {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // store current position
+    this.dy = this.dy + dy;
+
+    // move cursor relative
+    readline.moveCursor(this.stream, dx, dy);
+  }
+
+  // relative reset
+  cursorRelativeReset() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // move cursor to initial line
+    readline.moveCursor(this.stream, 0, -this.dy);
+
+    // first char
+    //readline.cursorTo(this.stream, 0, 0);
+
+    // reset counter
+    this.dy = 0;
+  }
+
+  // clear to the right from cursor
+  clearRight() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    readline.clearLine(this.stream, 1);
+  }
+
+  // clear the full line
+  clearLine() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    readline.clearLine(this.stream, 0);
+  }
+
+  // clear everyting beyond the current line
+  clearBottom() {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    readline.clearScreenDown(this.stream);
+  }
+
+  // add new line; increment counter
+  newline() {
+    this.stream.write('\n');
+    this.dy++;
+  }
+
+  // write content to output stream
+  // @TODO use string-width to strip length
+  write(s: string, rawWrite = false) {
+    // line wrapping enabled ? trim output
+    // this is just a fallback mechanism in case user enabled line-wrapping via options or set it to auto
+    if (this.linewrap === true && rawWrite === false) {
+      this.stream.write(s.substr(0, this.getWidth()));
+
+      // standard behaviour with disabled linewrapping
+    } else {
+      this.stream.write(s);
+    }
+  }
+
+  // control line wrapping
+  lineWrapping(enabled: boolean) {
+    if (!this.stream.isTTY) {
+      return;
+    }
+
+    // store state
+    this.linewrap = enabled;
+    if (enabled) {
+      this.stream.write('\x1B[?7h');
+    } else {
+      this.stream.write('\x1B[?7l');
+    }
+  }
+
+  // tty environment ?
+  isTTY() {
+    return this.stream.isTTY === true;
+  }
+
+  // get terminal width
+  getWidth() {
+    // set max width to 80 in tty-mode and 200 in notty-mode
+    return (this.stream as any).columns || (this.stream.isTTY ? 80 : 200);
+  }
+}
