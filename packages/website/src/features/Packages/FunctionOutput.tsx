7b9aa1391e4baa41a928bceb38b729eaad5544fc
Diego <gimbernat13@gmail.com>
2024-01-18 16:49:28 -0600

    renders objects and arrays results correctly

--- packages/website/src/features/Packages/FunctionOutput.tsx
+++ packages/website/src/features/Packages/FunctionOutput.tsx
@@ -1,23 +1,18 @@
 import { FC } from 'react';
-import { Box, Text, Tag } from '@chakra-ui/react';
+import { Box, Text, Tag, FormLabel, Flex } from '@chakra-ui/react';
 import { AbiParameter } from 'abitype';
+import { isArray, isObject } from 'lodash';
 
 export const FunctionOutput: FC<{
   output: AbiParameter | readonly AbiParameter[];
   result: any;
 }> = ({ output, result }) => {
-  const isArray = (value: any) => Array.isArray(value);
-
+  // Helper function to check if a value is an array of AbiParameters
   const isArrayOutput = (
     value: AbiParameter | readonly AbiParameter[]
-  ): value is readonly AbiParameter[] => {
-    return Array.isArray(value);
-  };
+  ): value is readonly AbiParameter[] => isArray(value);
 
-  const isObject = (_value: any) =>
-    _value && typeof _value === 'object' && _value.constructor === Object;
-
-
+  // Renders the output based on the type and structure of the item
   const renderOutput = (item: AbiParameter, value: any) => {
     if (item.type === 'tuple' && item.components) {
       return (
@@ -25,7 +20,7 @@ export const FunctionOutput: FC<{
           {item.components.map((component: AbiParameter, idx) => (
             <FunctionOutput
               key={idx}
-              output={[component]}
+              output={component}
               result={isArray(value) ? value[idx] : value}
             />
           ))}
@@ -33,33 +28,34 @@ export const FunctionOutput: FC<{
       );
     } else if (item.type === 'tuple[]' && item.components) {
       return isArray(value)
-        ? value.map((tupleItem: AbiParameter, tupleIndex) => (
-          <Box key={tupleIndex} pl="4">
-            {item.components.map(
-              (component: AbiParameter, compIdx: number) => (
+        ? value.map((tupleItem, tupleIndex) => (
+            <Box key={tupleIndex} pl="4">
+              {item.components.map((component, compIdx) => (
                 <FunctionOutput
                   key={compIdx}
-                  output={[component]}
-                  result={tupleItem[compIdx]}
+                  output={component}
+                  result={isArray(tupleItem) ? tupleItem[compIdx] : tupleItem}
                 />
-              )
-            )}
-          </Box>
-        ))
+              ))}
+            </Box>
+          ))
         : null;
-    } else if (isObject(value) && !Array.isArray(value)) {
-      // New handling for objects
-      return (
-        <Box pl="4">
-          {Object.keys(value).map((key, idx) => (
-            <Text key={idx}>
-              {key}: {String(value[key])}
-            </Text>
-          ))}
-        </Box>
-      );
-    } else if (item.type !== 'tuple[]' && item.type !== 'tuple[]') {
-      return <Text>{String(value)}</Text>;
+    } else {
+      // Check if the value is an object and item.name is a valid key
+      if (isObject(value) && item.name && item.name in value) {
+        console.log('value ', value);
+        return <Text>{String(value[item.name])}</Text>;
+      } else if (isArray(value)) {
+        // If the value is an array, render each element
+        return value.map((val, idx) => <Text key={idx}>{String(val)}</Text>);
+      } else {
+        // For primitive types or other cases, just convert the value to a string
+        return (
+          <Text pt="1" pb="3" fontSize="xs" color="whiteAlpha.900">
+            {String(result)}
+          </Text>
+        );
+      }
     }
   };
 
@@ -67,22 +63,27 @@ export const FunctionOutput: FC<{
     <>
       {isArrayOutput(output) ? (
         output.map((item, index) => (
-          <div key={index}>
-            <Text> {item.name}</Text>
+          <Box key={index}>
+            <Text>{item.name}</Text>
             <Tag colorScheme="teal" size={'sm'}>
               {item.type}
             </Tag>
             {renderOutput(item, result)}
-          </div>
+          </Box>
         ))
       ) : (
-        <div>
-          <Text> {output.name}</Text>
-          <Tag colorScheme="teal" size={'sm'}>
-            {output.type}
-          </Tag>
+        <Box>
+          <Flex alignItems={'center'} gap={1} fontSize="sm" mb={0}>
+            {output.name && <Text>{output.name}</Text>}
+            {output.internalType && (
+              <Text fontSize="xs" color="whiteAlpha.700">
+                {output.internalType}
+              </Text>
+            )}
+          </Flex>
+
           {renderOutput(output, result)}
-        </div>
+        </Box>
       )}
     </>
   );
