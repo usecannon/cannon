01fb62bd816f9aa7b570a944ed414d8dfff7972a
dbeal <git@danb.email>
2024-01-27 06:37:28 +0000

    Update packages/hardhat-cannon/src/task-names.ts
    Co-authored-by: Matías Lescano <mjlescano@users.noreply.github.com>

--- packages/hardhat-cannon/src/task-names.ts
+++ packages/hardhat-cannon/src/task-names.ts
@@ -8,6 +8,5 @@ export const TASK_RUN = `${TASK_PREFIX}:run`;
 export const TASK_TEST = `${TASK_PREFIX}:test`;
 
 export const SUBTASK_LOAD_PACKAGE_DEFINITION = `${TASK_PREFIX}:load-package-definition`;
-export const SUBTASK_LOAD_DEPLOY = `${TASK_PREFIX}:load-deploy`;
 export const SUBTASK_GET_ARTIFACT = `${TASK_PREFIX}:get-artifact`;
 export const SUBTASK_RUN_ANVIL_NODE = `${TASK_PREFIX}:run-anvil-node`;
