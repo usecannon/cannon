c0b6255da02c59df0e862f68b94e4028e5c52223
FuzzB0t <christopherpagnusatt@gmail.com>
2024-02-09 19:06:13 -0600

    testing fix for CI registry tests

--- packages/registry/test/helpers/assert-revert.ts
+++ packages/registry/test/helpers/assert-revert.ts
@@ -2,6 +2,6 @@ import { rejects } from 'assert/strict';
 
 export default async function assertRevert(fn: () => Promise<unknown>, errorMessage: string) {
   await rejects(fn, {
-    message: `VM Exception while processing transaction: reverted with custom error '${errorMessage}'`,
+    message: `VM Exception while processing transaction: reverted with an unrecognized custom error '${errorMessage}'`,
   });
 }
