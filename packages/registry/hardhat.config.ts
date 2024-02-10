34d669db83183d72dbfb511e0010b105272acce5
FuzzB0t <christopherpagnusatt@gmail.com>
2024-02-09 19:09:47 -0600

    reverting registry test changes

--- packages/registry/hardhat.config.ts
+++ packages/registry/hardhat.config.ts
@@ -7,7 +7,7 @@ import 'hardhat-gas-reporter';
 import 'hardhat-contract-sizer';
 import '@nomiclabs/hardhat-ethers';
 
-import 'hardhat-cannon/src/index';
+import 'hardhat-cannon';
 
 dotenv.config();
 
