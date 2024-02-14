cc1863744d5b4b0a531ee236437df64f3b3e5fdf
Matías <mjlescano@protonmail.com>
2024-02-14 18:42:41 -0300

    fix getContract util

--- packages/hardhat-cannon/src/utils/index.ts
+++ packages/hardhat-cannon/src/utils/index.ts
@@ -12,3 +12,9 @@ export function getAllContractDatas() {
   if (!hre.cannon.outputs) throw new Error('There are no cannon artifacts present');
   return getAllContractDatasFromOutputs(hre.cannon.outputs);
 }
+
+/** Get the abi and address from the built outputs */
+export async function getContract(contractName: string) {
+  const contract = getContractData(contractName);
+  return { address: contract.address, abi: contract.abi };
+}
