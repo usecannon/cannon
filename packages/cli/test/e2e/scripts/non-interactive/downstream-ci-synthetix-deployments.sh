22c50eb1dfd295864904c1ae2886f95eba16483a
FuzzB0t <30664234+FuzzB0t@users.noreply.github.com>
2024-02-13 22:51:00 -0600

    fix(cli): Adding npm pack to e2e test suite (#764)

--- packages/cli/test/e2e/scripts/non-interactive/downstream-ci-synthetix-deployments.sh
+++ packages/cli/test/e2e/scripts/non-interactive/downstream-ci-synthetix-deployments.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 git clone --depth=1 https://github.com/Synthetixio/synthetix-deployments
 cd synthetix-deployments
-yarn link $CANNON_REPO_DIR/packages/builder 
-yarn link $CANNON_REPO_DIR/packages/cli 
-yarn link $CANNON_REPO_DIR/packages/hardhat-cannon
 yarn
+
+cli=$(npm pack $CANNON_REPO_DIR/packages/cli)
+builder=$(npm pack $CANNON_REPO_DIR/packages/builder)
+
+yarn add $cli
+yarn add $builder
 
 yarn cannon build omnibus-base-mainnet-andromeda.toml \
   --port 8545 \
