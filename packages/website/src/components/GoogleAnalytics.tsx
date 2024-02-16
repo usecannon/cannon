3f0dfc0ae584cc5fb2d7a87936b61009ea065c9e
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-02-16 10:13:35 -0500

    ts fix

--- packages/website/src/components/GoogleAnalytics.tsx
+++ packages/website/src/components/GoogleAnalytics.tsx
@@ -1,7 +1,11 @@
 // components/GoogleAnalytics.js
 import Script from 'next/script';
 
-export default function GoogleAnalytics({ measurementId }) {
+export default function GoogleAnalytics({
+  measurementId,
+}: {
+  measurementId: string;
+}) {
   return (
     <>
       <Script
