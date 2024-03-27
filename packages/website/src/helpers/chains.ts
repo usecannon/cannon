c80524dc500d161b8a03ae564094daccfd8063f2
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-03-27 11:14:45 -0400

    chore: Bump wagmi and viem versions (#843)
    Co-authored-by: FuzzB0t <christopherpagnusatt@gmail.com>

--- packages/website/src/helpers/chains.ts
+++ packages/website/src/helpers/chains.ts
@@ -1,8 +1,8 @@
-import * as chains from 'wagmi/chains';
+import * as chains from 'viem/chains';
 
 // For enrichment if necessary
 //import { set } from 'lodash';
 //set(chains, 'baseSepolia.blockExplorers.default.url', 'https://sepolia.basescan.org');
 
-export * from 'wagmi/chains';
+export * from 'viem/chains';
 export default chains;
