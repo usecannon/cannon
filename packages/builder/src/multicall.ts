bc6d101853c4d4f3807c573b8990fcfa7b00bde6
FuzzB0t <30664234+FuzzB0t@users.noreply.github.com>
2024-04-04 05:54:56 -0400

    fix: misc fixes from name respec (#828)
    Co-authored-by: dbeal <git@danb.email>

--- packages/builder/src/multicall.ts
+++ packages/builder/src/multicall.ts
@@ -3,7 +3,7 @@ import MulticallABI from './abis/Multicall';
 
 const MULTICALL_ADDRESS = '0xE2C5658cC5C448B48141168f3e475dF8f65A1e3e';
 
-interface txData {
+export interface txData {
   abi: viem.Abi;
   address: viem.Address;
   functionName: string;
