50a9ade842c7019dcedabb796f320096ea92bf6d
Jobelo Andres Quintero Rodriguez <ignusmart@gmail.com>
2024-04-12 13:46:21 -0500

    feat(website): provisioned packages code (#890)
    Co-authored-by: Noah Litvin <335975+noahlitvin@users.noreply.github.com>

--- packages/website/src/features/Packages/CodeExplorer.tsx
+++ packages/website/src/features/Packages/CodeExplorer.tsx
@@ -15,6 +15,7 @@ import { CodePreview } from '@/components/CodePreview';
 import { useQueryIpfsData } from '@/hooks/ipfs';
 import { DownloadIcon, InfoOutlineIcon } from '@chakra-ui/icons';
 import { CustomSpinner } from '@/components/CustomSpinner';
+import { isEmpty } from 'lodash';
 
 const handleDownload = (content: JSON, filename: string) => {
   const blob = new Blob([JSON.stringify(content, null, 2)], {
@@ -30,9 +31,64 @@ const handleDownload = (content: JSON, filename: string) => {
   URL.revokeObjectURL(url);
 };
 
+const PackageButton: FC<{
+  name: string;
+  selected: boolean;
+  onClick: () => void;
+}> = ({ name, selected, onClick }) => {
+  return (
+    <Button
+      color="white"
+      borderWidth="2px"
+      borderRadius="md"
+      variant="outline"
+      aria-label="contract name"
+      boxShadow="lg"
+      flexShrink={0}
+      background={selected ? 'teal.900' : 'gray.700'}
+      borderColor={selected ? 'teal.600' : 'gray.600'}
+      _hover={
+        selected
+          ? {
+              background: 'teal.800',
+              borderColor: 'teal.500',
+            }
+          : {
+              background: 'gray.600',
+              borderColor: 'teal.500',
+            }
+      }
+      mr={3}
+      height="36px"
+      px={3}
+      onClick={onClick}
+    >
+      <Box textAlign="left">
+        <Heading
+          fontWeight="500"
+          size="sm"
+          color="gray.200"
+          letterSpacing="0.1px"
+        >
+          {name}
+        </Heading>
+      </Box>
+    </Button>
+  );
+};
+
 export const CodeExplorer: FC<{
   variant: any;
-}> = ({ variant }) => {
+  name: string;
+}> = ({ variant, name }) => {
+  // For the main package, the key is -1
+  const [selectedPackage, setSelectedPackage] = useState<{
+    name: string;
+    key: number;
+  }>({
+    name,
+    key: -1,
+  });
   const { data: metadata } = useQueryIpfsData(
     variant?.meta_url,
     !!variant?.meta_url
@@ -43,6 +99,41 @@ export const CodeExplorer: FC<{
     !!variant?.deploy_url
   );
 
+  // Provisioned packages could be inside the "provision" (old) or "clone" (current) key
+  // So we check both in order to keep backwards compatibility
+  const provisionedPackagesKeys =
+    deploymentData?.data?.def?.provision || deploymentData?.data?.def?.clone
+      ? Object.keys(
+          deploymentData?.data?.def?.provision ||
+            deploymentData?.data?.def?.clone
+        )
+      : [];
+  const provisionArtifacts = provisionedPackagesKeys.map((k: string) => {
+    return {
+      name: k,
+      artifacts: (
+        deploymentData?.data?.state[`provision.${k}`] ||
+        deploymentData?.data?.state[`clone.${k}`]
+      )?.artifacts,
+    };
+  });
+
+  const {
+    data: provisionedPackageData,
+    isLoading: isLoadingProvisionedPackageData,
+  } = useQueryIpfsData(
+    provisionArtifacts[selectedPackage.key]?.artifacts?.imports[
+      selectedPackage.name
+    ]?.url,
+    !!provisionArtifacts[selectedPackage.key]?.artifacts?.imports[
+      selectedPackage.name
+    ]?.url
+  );
+
+  const provisionedMiscUrl = provisionedPackageData?.miscUrl;
+  const { data: provisionedMiscData, isLoading: isLoadingProvisionedMiscData } =
+    useQueryIpfsData(provisionedMiscUrl, !!provisionedMiscUrl);
+
   let miscUrl: string | undefined;
   if (deploymentData?.data) {
     miscUrl =
@@ -51,11 +142,25 @@ export const CodeExplorer: FC<{
         : JSON.parse(JSON.stringify((deploymentData as any)?.data))?.miscUrl;
   }
 
-  const miscData = useQueryIpfsData(miscUrl, !!miscUrl);
+  const { data: miscData, isLoading: isLoadingMiscData } = useQueryIpfsData(
+    miscUrl,
+    !!miscUrl
+  );
+
+  const isSelectedPackage = ({ key, name }: { key: number; name: string }) =>
+    selectedPackage.key === key && selectedPackage.name === name;
 
   useEffect(() => {
-    if (miscData?.data) {
-      const firstArtifact = Object.entries(miscData.data.artifacts).sort(
+    // If the selected package is the main package, select the first source code
+    const selectedMiscData = isSelectedPackage({
+      name,
+      key: -1,
+    })
+      ? miscData
+      : provisionedMiscData;
+
+    if (selectedMiscData) {
+      const firstArtifact = Object.entries(selectedMiscData.artifacts).sort(
         ([keyA], [keyB]) => {
           const countA = (keyA.match(/:/g) || []).length;
           const countB = (keyB.match(/:/g) || []).length;
@@ -85,7 +190,7 @@ export const CodeExplorer: FC<{
         }
       }
     }
-  }, [miscData?.data]);
+  }, [miscData, provisionedMiscData]);
 
   const isSmall = useBreakpointValue({
     base: true,
@@ -97,11 +202,66 @@ export const CodeExplorer: FC<{
   const [selectedLanguage, setSelectedLanguage] = useState('');
   const [selectedKey, setSelectedKey] = useState('');
 
-  const artifacts = miscData?.data && Object.entries(miscData?.data.artifacts);
+  const artifacts =
+    // If the selected package is the main package, use the misc data
+    isSelectedPackage({ name, key: -1 })
+      ? miscData && Object.entries(miscData?.artifacts)
+      : provisionedMiscData && Object.entries(provisionedMiscData?.artifacts);
+
+  const handleSelectPackage = (v: { name: string; key: number }) => {
+    setSelectedPackage(v);
+  };
+
+  const isLoading =
+    !miscData ||
+    isLoadingProvisionedPackageData ||
+    isLoadingMiscData ||
+    isLoadingProvisionedMiscData;
+
+  useEffect(() => {
+    if (isEmpty(miscData?.artifacts) && provisionedPackagesKeys.length) {
+      setSelectedPackage({
+        name: provisionedPackagesKeys[0],
+        key: 0,
+      });
+    }
+  }, [miscData]);
 
   return (
     <Flex flex="1" direction="column" maxHeight="100%" maxWidth="100%">
-      {!miscData?.data ? (
+      {!!provisionedPackagesKeys.length && (
+        <Flex
+          top="0"
+          zIndex={3}
+          bg="gray.900"
+          position={{ md: 'sticky' }}
+          overflowX="scroll"
+          overflowY="hidden"
+          maxW="100%"
+          p={2}
+          borderBottom="1px solid"
+          borderColor="gray.800"
+          flexWrap="nowrap"
+        >
+          {!isEmpty(miscData?.artifacts) && (
+            <PackageButton
+              key={-1}
+              name={name}
+              selected={isSelectedPackage({ name, key: -1 })}
+              onClick={() => handleSelectPackage({ name, key: -1 })}
+            />
+          )}
+          {provisionedPackagesKeys.map((k: string, i: number) => (
+            <PackageButton
+              key={k}
+              name={k}
+              selected={isSelectedPackage({ name: k, key: i })}
+              onClick={() => handleSelectPackage({ name: k, key: i })}
+            />
+          ))}
+        </Flex>
+      )}
+      {isLoading ? (
         <CustomSpinner m="auto" />
       ) : artifacts?.length ? (
         <Flex flex="1" direction={['column', 'column', 'row']}>
