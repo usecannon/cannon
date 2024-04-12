102b35323f45a7bee21334869bb31093a50223b9
FuzzB0t <christopherpagnusatt@gmail.com>
2024-04-11 22:05:54 -0600

    misc fixes for e2e tests

--- packages/cli/src/settings.ts
+++ packages/cli/src/settings.ts
@@ -9,6 +9,7 @@ import untildify from 'untildify';
 
 import { CLI_SETTINGS_STORE, DEFAULT_CANNON_DIRECTORY, DEFAULT_REGISTRY_CONFIG } from './constants';
 import { filterSettings, checkAndNormalizePrivateKey } from './helpers';
+import { getCannonRepoRegistryUrl } from '@usecannon/builder';
 
 const debug = Debug('cannon:cli:settings');
 
@@ -130,7 +131,7 @@ function cannonSettingsSchema(fileSettings: Omit<CliSettings, 'cannonDirectory'>
       .string()
       .url()
       .optional()
-      .default(fileSettings.ipfsUrl as string),
+      .default(fileSettings.ipfsUrl || getCannonRepoRegistryUrl()),
     CANNON_PUBLISH_IPFS_URL: z
       .string()
       .url()
