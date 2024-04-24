a04e9dc3cbede7ca6dbf5f3c0cd457f40257f0a1
dbeal <git@danb.email>
2024-04-25 00:15:38 +0900

    feat: Repo controller (#920)

---
+++ packages/indexer/src/db.ts
@@ -0,0 +1,21 @@
+export const RKEY_LAST_IDX = 'reg:lastBlock';
+export const RKEY_LAST_UPDATED = 'reg:lastTimestamp';
+export const RKEY_ADDRESS_TO_PACKAGE = 'reg:addressToPackage';
+export const RKEY_TRANSACTION_TO_PACKAGE = 'reg:transactionToPackage';
+export const RKEY_PACKAGE_RELATION = 'reg:packageRelation';
+
+export const RKEY_RETRY_PROCESS_PACKAGE = 'reg:retryPackage';
+
+export const RKEY_COUNTER_STEP_TYPE_PREFIX = 'reg:steps:counter';
+
+export const RKEY_PACKAGE_NAMES = 'reg:packages';
+
+export const RKEY_FEES_PAID = 'reg:feesPaid';
+
+export const RKEY_SELECTOR_LIST = 'reg:selectors';
+export const RKEY_SELECTOR_CONTRACT = 'reg:contracts:selectors';
+
+export const RKEY_TS_PACKAGE_COUNT = 'reg:packages:ts';
+export const RKEY_TS_CONTRACT_COUNT = 'reg:artifacts:contracts:ts';
+export const RKEY_TS_TRANSACTION_COUNT = 'reg:artifacts:txns:ts';
+export const RKEY_TS_FEES_PAID = 'reg:feesPaid:ts';
