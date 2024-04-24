247e550e4336c6ec7ee63e0d84bf64698814d49f
Matías <mjlescano@protonmail.com>
2024-04-25 03:19:34 +0900

    fix lint

--- packages/website/src/hooks/backend.ts
+++ packages/website/src/hooks/backend.ts
@@ -195,7 +195,6 @@ export function useTxnStager(
 
   // must not have already signed in order to sign
   const existingSigsCount = alreadyStaged ? alreadyStaged.sigs.length : 0;
-  const currentNonce = safeTxn._nonce != null && nonce == BigInt(safeTxn._nonce);
   const isSigner =
     reads.isSuccess && !reads.isFetching && !reads.isRefetching ? (reads.data![2].result as unknown as boolean) : false;
   const alreadySigned = existingSigsCount >= requiredSigs;
