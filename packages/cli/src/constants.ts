dfacc1af3ec9ae633f83b34b89824720cd7b8eb2
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-04-24 13:45:59 -0400

    fix import

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,5 +1,5 @@
-import os from 'node:os';
-import path from 'node:path';
+import os from 'os';
+import path from 'path';
 import * as viem from 'viem';
 
 export const ANVIL_FIRST_ADDRESS: viem.Address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
