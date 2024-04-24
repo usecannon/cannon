b9e5e439bfef84d15c125375e9a52c47c08b59cb
Daniel Beal <git@dbeal.dev>
2024-04-24 22:28:10 +0900

    put in most of the needed logic

--- packages/website/src/features/Deploy/PrepareNetwork.tsx
+++ packages/website/src/features/Deploy/PrepareNetwork.tsx
@@ -1,7 +1,55 @@
 import { Flex, Container, Heading, Text, Box, Button } from '@chakra-ui/react';
 import { CustomLinkButton } from '../HomePage/HomePage';
+import {
+  useBalance,
+  useGasPrice,
+  usePrepareTransactionRequest,
+  usePublicClient,
+  useSendTransaction,
+} from 'wagmi';
+
+import * as onchainStore from '../../helpers/onchain-store';
+import * as multicallForwarder from '../../helpers/trusted-multicall-forwarder';
 
 export default function PrepareNetwork() {
+  const ARACHNID_CREATOR = '0x3fab184622dc19b6109349b94811493bf2a45362';
+
+  const arachnidCreatorBalance = useBalance({
+    address: ARACHNID_CREATOR,
+  });
+
+  const gasPrice = useGasPrice();
+
+  const publicClient = usePublicClient();
+
+  const deployArachnidCreate2 = usePrepareTransactionRequest({
+    to: ARACHNID_CREATOR,
+    value:
+      (gasPrice.data ?? BigInt(0)) * BigInt(110000) -
+      (arachnidCreatorBalance.data?.value ?? BigInt(0)),
+  });
+
+  const deployKvStore = usePrepareTransactionRequest(onchainStore.deployTxn);
+
+  const deployMulticallForwarder = usePrepareTransactionRequest(
+    multicallForwarder.deployTxn
+  );
+
+  const execTxnArachnid = useSendTransaction({
+    mutation: {
+      onSuccess: async () => {
+        const hash = await publicClient!.sendRawTransaction({
+          serializedTransaction:
+            '0x604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3',
+        });
+
+        await publicClient!.waitForTransactionReceipt({ hash });
+      },
+    },
+  });
+
+  const execTxn = useSendTransaction();
+
   return (
     <Flex height="100%" bg="black">
       <Container maxW="container.xl" my="auto" py={8}>
@@ -32,9 +80,21 @@ export default function PrepareNetwork() {
             </Heading>
             <Text fontSize="sm" color="gray.300" mb={3}>
               This allows contracts to be deployed at consistent addresses,
-              determined based on their source code.
+              determined based on their source code. NOTE: this contract is
+              deployed by sending a small amount of ETH to a dedicated
+              deployment address. The contract will be deployed immediately
+              after funds are sent.
             </Text>
-            <CustomLinkButton href="#" size="sm" colorScheme="teal" disabled>
+            <CustomLinkButton
+              href="#"
+              size="sm"
+              colorScheme="teal"
+              disabled
+              // NOTE: seems wagmi types are borked again
+              onClick={execTxnArachnid.sendTransaction(
+                deployArachnidCreate2.data as any
+              )}
+            >
               Deployed
             </CustomLinkButton>
           </Box>
@@ -53,7 +113,13 @@ export default function PrepareNetwork() {
               This allows the deployer to record IPFS and git hashes onchain to
               verify the integrity of upgrades.
             </Text>
-            <CustomLinkButton href="#" size="sm" colorScheme="teal">
+            <CustomLinkButton
+              href="#"
+              size="sm"
+              colorScheme="teal"
+              // NOTE: seems wagmi types are borked again
+              onClick={execTxn.sendTransaction(deployKvStore.data as any)}
+            >
               Deploy Contract
             </CustomLinkButton>
           </Box>
@@ -72,7 +138,15 @@ export default function PrepareNetwork() {
               This allows users to create atomic batch transactions across
               integrated protocols, like the Cannon Registry.
             </Text>
-            <CustomLinkButton href="#" size="sm" colorScheme="teal">
+            <CustomLinkButton
+              href="#"
+              size="sm"
+              colorScheme="teal"
+              // NOTE: seems wagmi types are borked again
+              onClick={execTxn.sendTransaction(
+                deployMulticallForwarder.data as any
+              )}
+            >
               Deploy Contract
             </CustomLinkButton>
           </Box>
