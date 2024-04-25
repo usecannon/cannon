3f738d4a2e722d00dea8fac15329e365de997556
Matías <mjlescano@users.noreply.github.com>
2024-04-25 18:25:27 +0900

    fix(website): usage of isIpfsGateway helper on website (#927)

--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -1,18 +1,17 @@
 'use client';
-import '@/styles/globals.css';
 
-import { Providers } from './providers';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
-import { Miriam_Libre, Inter, Roboto_Mono } from 'next/font/google';
-import { Flex } from '@chakra-ui/react';
-import { Header } from '@/features/Header/Header';
-import { Footer } from '@/features/Footer/Footer';
 import { Console } from '@/features/Console/Console';
-import { Analytics } from '@vercel/analytics/react';
-import { ReactNode, useEffect } from 'react';
-import axios, { AxiosError } from 'axios';
+import { Footer } from '@/features/Footer/Footer';
+import { Header } from '@/features/Header/Header';
 import { useStore } from '@/helpers/store';
-import { parse as parseUrl } from 'simple-url';
+import { Flex } from '@chakra-ui/react';
+import { isIpfsGateway } from '@usecannon/builder';
+import { Analytics } from '@vercel/analytics/react';
+import { Inter, Miriam_Libre, Roboto_Mono } from 'next/font/google';
+import { ReactNode, useEffect } from 'react';
+import { Providers } from './providers';
+import '@/styles/globals.css';
 
 const miriam = Miriam_Libre({
   subsets: ['latin'],
@@ -30,43 +29,14 @@ const mono = Roboto_Mono({
   display: 'swap',
 });
 
-async function isIpfsGateway(ipfsUrl: string) {
-  let isGateway = true;
-  try {
-    ipfsUrl = ipfsUrl.endsWith('/') ? ipfsUrl : ipfsUrl + '/';
-    const parsedUrl = parseUrl(ipfsUrl);
-    const headers: { [k: string]: string } = {};
-
-    if (parsedUrl.auth) {
-      const [username, password] = parsedUrl.auth.split(':');
-      headers['Authorization'] = `Basic ${btoa(`${username}:${password}`)}`;
-    }
-    await axios.post(ipfsUrl + 'api/v0/cat', null, {
-      headers,
-      timeout: 15 * 1000,
-    });
-  } catch (err: unknown) {
-    if (
-      err instanceof AxiosError &&
-      err.response?.status === 400 &&
-      err.response?.data.includes('argument "ipfs-path" is required')
-    ) {
-      isGateway = false;
-    }
-  }
-
-  return isGateway;
-}
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   const settings = useStore((s) => s.settings);
   const setSettings = useStore((s) => s.setSettings);
 
   useEffect(() => {
     async function fetch() {
-      setSettings({
-        isIpfsGateway: await isIpfsGateway(settings.ipfsApiUrl),
-      });
+      const isGateway = await isIpfsGateway(settings.ipfsApiUrl);
+      setSettings({ isIpfsGateway: isGateway });
     }
 
     void fetch();
