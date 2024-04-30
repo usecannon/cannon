06f52b8108c4dcb016cd7c7eaf136de01004fee3
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-04-30 07:59:57 -0400

    feat(website): subgraph update (#942)

--- packages/website/src/graphql/ApolloClient.ts
+++ packages/website/src/graphql/ApolloClient.ts
@@ -1,12 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/noahlitvin/cannon-registry-mainnet',
+  uri: 'https://gateway-arbitrum.network.thegraph.com/api/ffe77687bd873a8d0426b3aeba4b8dc5/subgraphs/id/4VKdYs7BmLDxZHffkusKRknCmPkgw7Aa4Msx4kP2sETN',
   cache: new InMemoryCache(),
 });
 
 export const apolloClientOptimism = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/noahlitvin/cannon-registry-optimism',
+  uri: 'https://gateway-arbitrum.network.thegraph.com/api/ffe77687bd873a8d0426b3aeba4b8dc5/subgraphs/id/JE9WYVo5XUo4FF67wURm8GUD1bxmiBBrzG1GJVTb7zQ8',
   cache: new InMemoryCache(),
 });
 
