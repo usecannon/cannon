7949ebb990e071106d8d6d891d523597690b15fc
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-05-30 03:58:22 -0400

    fix(website): Prevent FOUC (#1067)
    Co-authored-by: dbeal <git@danb.email>

--- packages/website/src/pages/_document.tsx
+++ packages/website/src/pages/_document.tsx
@@ -39,13 +39,16 @@ export default class CustomDocument extends Document {
         <Head>
           <GoogleAnalytics measurementId="G-C96791F6NC" />
           <style>{`
-            body {
+            body.fouc-prevention {
               background-color: rgb(14, 17, 22);
+            }
+            body.fouc-prevention * {
+              display: none !important;
             }
           `}</style>
           <link rel="icon" href="/favicon.ico" sizes="any" />
         </Head>
-        <body>
+        <body className="fouc-prevention">
           <ColorModeScript />
           <Main />
           <NextScript />
