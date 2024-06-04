bbd88ea2c99f744497c95a38766b4899b719a6ee
Jobelo Andres Quintero Rodriguez <ignusmart@gmail.com>
2024-06-04 08:19:34 -0500

    Update packages/website/cypress/step-definitions/wallet-connector.ts
    Co-authored-by: saeta.eth <saetaeth@proton.me>

--- packages/website/cypress/step-definitions/wallet-connector.ts
+++ packages/website/cypress/step-definitions/wallet-connector.ts
@@ -2,6 +2,6 @@ import { Given } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('Wallet is connected', () => {
   cy.window().then((window) => {
-    window.localStorage.setItem('e2e-allet-connected', 'true');
+    window.localStorage.setItem('e2e-wallet-connected', 'true');
   });
 });
