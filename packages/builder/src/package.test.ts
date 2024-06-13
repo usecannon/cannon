486a39a5ed461b3cfcd5f6b826e0c0fedf9f1b68
Matías <mjlescano@users.noreply.github.com>
2024-06-13 13:45:31 +0900

    fix(builder): package length check (#1114)

--- packages/builder/src/package.test.ts
+++ packages/builder/src/package.test.ts
@@ -1,8 +1,8 @@
+import { CannonStorage } from './';
 import { IPFSLoader } from './loader';
-import { InMemoryRegistry } from './registry';
 import { PackageReference, publishPackage } from './package';
+import { InMemoryRegistry } from './registry';
 import { DeploymentInfo } from './types';
-import { CannonStorage } from '.';
 
 jest.mock('./loader');
 
@@ -27,7 +27,7 @@ describe('package.ts', () => {
       expect(ref).toHaveProperty('fullPackageRef', fullPackageRef);
     });
 
-    test.each([['a'], ['aa'], ['-aa'], ['some_package'], ['super-long-package-invalid-namee']])(
+    test.each([['a'], ['aa'], ['-aa'], ['some_package'], ['super-long-package-invalid-nameee']])(
       'throws an error with "%s"',
       (packageRef) => {
         expect(() => {
