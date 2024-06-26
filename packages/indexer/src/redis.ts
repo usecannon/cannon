a76b08b794081ac6c73717016dcee0ed3a4e8ff9
Matías <mjlescano@users.noreply.github.com>
2024-06-26 15:21:05 +0900

    chore(indexer): simple refactors (#1168)

---
+++ packages/indexer/src/redis.ts
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+import { config } from './config';
+
+export type ActualRedisClientType = ReturnType<typeof createClient>;
+
+export async function useRedis() {
+  const client = createClient({
+    url: config.REDIS_URL,
+    socket: {
+      reconnectStrategy: (retries, err) => {
+        // After 5 retries, halt the server
+        if (retries > 5) {
+          // eslint-disable-next-line no-console
+          console.error(err);
+          process.exit(1);
+        }
+
+        return 5_000; // retry after 5 secs
+      },
+    },
+  });
+
+  client.on('ready', () => {
+    // eslint-disable-next-line no-console
+    console.log(' · redis connected ·');
+  });
+
+  client.on('error', (err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
+
+  await client.connect();
+
+  return client;
+}
