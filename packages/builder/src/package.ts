099b56405f1749c30c22bde411e812e83a804145
FuzzB0t <christopherpagnusatt@gmail.com>
2024-07-05 09:20:45 -0600

    lint fixes

--- packages/builder/src/package.ts
+++ packages/builder/src/package.ts
@@ -87,13 +87,13 @@ export class PackageReference {
       throw new Error(`Package reference "${ref}" is too long. Package name exceeds 32 bytes`);
     }
 
-    
     if (match.groups.version) res.version = match.groups.version;
-    
+
     const versionSize = new Blob([res.version!]).size;
     if (!(versionSize <= 32)) {
       throw new Error(`Package reference "${ref}" is too long. Package variant exceeds 32 bytes`);
     }
+
     if (match.groups.preset) res.preset = match.groups.preset;
 
     return res;
