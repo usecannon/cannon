c85e35c969f401c4e69309ad9a21a35b1864a94b
Matías <mjlescano@protonmail.com>
2024-07-10 10:30:22 +0200

    add fuzzy search suggestions for cannonfile templates

--- packages/builder/src/utils/template.ts
+++ packages/builder/src/utils/template.ts
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js';
 import _ from 'lodash';
 
 export function template(str: string, data = {}) {
@@ -6,8 +7,40 @@ export function template(str: string, data = {}) {
   } catch (err) {
     if (err instanceof Error) {
       err.message += ` at ${str}`;
+
+      if (err.message.includes('Cannot read properties of undefined')) {
+        const match = err.message.match(/\(reading '([^']+)'\)/);
+        if (match && match[1]) {
+          const desiredKey = match[1];
+          const allKeys = Array.from(getAllKeys(data));
+          const results = fuzzySearch(allKeys, desiredKey);
+          if (results.length) {
+            err.message += '\n\n Did you mean?';
+            for (const res of results) err.message += `\n    ${res}`;
+          }
+        }
+      }
     }
 
     throw err;
   }
+}
+
+function getAllKeys(obj: { [key: string]: any }, parentKey = '', keys: Set<string> = new Set()) {
+  for (const key of Object.keys(obj)) {
+    const fullKey = parentKey ? `${parentKey}.${key}` : key;
+    keys.add(fullKey);
+    if (_.isPlainObject(obj[key])) getAllKeys(obj[key], fullKey, keys);
+  }
+
+  return keys;
+}
+
+function fuzzySearch(data: string[], query: string) {
+  const searcher = new Fuse<string>(data, {
+    ignoreLocation: true,
+    threshold: 0.3,
+  });
+
+  return searcher.search(query).map(({ item }) => item);
 }
