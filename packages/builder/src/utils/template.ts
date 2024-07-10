c4544951ff349a072bd0892462775d3cbff75234
Matías <mjlescano@protonmail.com>
2024-07-10 17:35:15 +0200

    update template fn everywhere

--- packages/builder/src/utils/template.ts
+++ packages/builder/src/utils/template.ts
@@ -1,29 +1,37 @@
 import Fuse from 'fuse.js';
 import _ from 'lodash';
 
-export function template(str: string, data = {}) {
-  try {
-    return _.template(str)(data);
-  } catch (err) {
-    if (err instanceof Error) {
-      err.message += ` at ${str}`;
+import type { TemplateOptions } from 'lodash';
 
-      if (err.message.includes('Cannot read properties of undefined')) {
-        const match = err.message.match(/\(reading '([^']+)'\)/);
-        if (match && match[1]) {
-          const desiredKey = match[1];
-          const allKeys = Array.from(getAllKeys(data));
-          const results = fuzzySearch(allKeys, desiredKey);
-          if (results.length) {
-            err.message += "\n\n Here's a list of some fields available in this context, did you mean one of these?";
-            for (const res of results) err.message += `\n    ${res}`;
+export function template(str: string, options?: TemplateOptions) {
+  const render = _.template(str, options);
+
+  return (data?: object) => {
+    try {
+      return render(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        err.message += ` at ${str}`;
+
+        // Do a fuzzy search in the given context, in the case the user did a typo
+        // we look for something close in the current data context object.
+        if (data && err.message.includes('Cannot read properties of undefined')) {
+          const match = err.message.match(/\(reading '([^']+)'\)/);
+          if (match && match[1]) {
+            const desiredKey = match[1];
+            const allKeys = Array.from(getAllKeys(data));
+            const results = fuzzySearch(allKeys, desiredKey);
+            if (results.length) {
+              err.message += "\n\n Here's a list of some fields available in this context, did you mean one of these?";
+              for (const res of results) err.message += `\n    ${res}`;
+            }
           }
         }
       }
-    }
 
-    throw err;
-  }
+      throw err;
+    }
+  };
 }
 
 function getAllKeys(obj: { [key: string]: any }, parentKey = '', keys: Set<string> = new Set()) {
