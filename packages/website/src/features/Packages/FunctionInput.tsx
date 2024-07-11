f0cbc74eab52839e2cf6ce4c04a8aa762eabd47f
FuzzB0t <30664234+FuzzB0t@users.noreply.github.com>
2024-07-10 22:09:03 -0700

    fix(website): Normalize empty strings inside array for contract calls (#1188)
    Co-authored-by: Matías <mjlescano@protonmail.com>

--- packages/website/src/features/Packages/FunctionInput.tsx
+++ packages/website/src/features/Packages/FunctionInput.tsx
@@ -1,18 +1,24 @@
-import { FC, useEffect, useMemo, useState } from 'react';
-import { AbiParameter } from 'abitype';
-import { Box, Flex, Link } from '@chakra-ui/react';
-import { BoolInput } from '@/features/Packages/FunctionInput/BoolInput';
 import { AddressInput } from '@/features/Packages/FunctionInput/AddressInput';
-import { NumberInput } from '@/features/Packages/FunctionInput/NumberInput';
+import { BoolInput } from '@/features/Packages/FunctionInput/BoolInput';
 import { DefaultInput } from '@/features/Packages/FunctionInput/DefaultInput';
+import { NumberInput } from '@/features/Packages/FunctionInput/NumberInput';
 import { AddIcon, CloseIcon } from '@chakra-ui/icons';
+import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { AbiParameter } from 'abitype';
+import { FC, useEffect, useMemo, useState } from 'react';
 import TupleInput from './FunctionInput/TupleInput';
 
-export const FunctionInput: FC<{
+interface Props {
   input: AbiParameter;
   valueUpdated: (value: any) => void;
   initialValue?: any;
-}> = ({ input, valueUpdated, initialValue }) => {
+}
+
+export const FunctionInput: FC<Props> = ({
+  input,
+  valueUpdated,
+  initialValue,
+}) => {
   const getDefaultValue = () => {
     if (input.type.startsWith('int')) return '0';
     if (input.type.startsWith('uint')) return '0';
@@ -23,7 +29,15 @@ export const FunctionInput: FC<{
     { val: getDefaultValue() },
   ]);
 
-  // const getValue = (index: number) => (isArray ? dataArray[index] : input);
+  const updateValue = (value: any) => {
+    // When getting a single empty string array, the value should be an empty array
+    // so the user is able to have an empty value
+    const result =
+      Array.isArray(value) && value.length === 1 && value[0] === ''
+        ? []
+        : value;
+    valueUpdated(result);
+  };
 
   const add = () => {
     setDataArray([...dataArray, { val: getDefaultValue() }]);
@@ -35,18 +49,15 @@ export const FunctionInput: FC<{
 
   useEffect(() => {
     if (!isArray) return;
-    valueUpdated(dataArray.map((item) => item.val));
+    updateValue(dataArray.map((item) => item.val));
   }, [dataArray, isArray]);
 
   useEffect(() => {
-    if (initialValue && !isArray) {
-      valueUpdated(initialValue);
-    } else if (initialValue && isArray) {
-      setDataArray(
-        initialValue.map((val: any) => {
-          return { val };
-        })
-      );
+    if (!initialValue) return;
+    if (isArray) {
+      setDataArray(initialValue.map((val: any) => ({ val })));
+    } else {
+      updateValue(initialValue);
     }
   }, []);
 
@@ -56,7 +67,7 @@ export const FunctionInput: FC<{
       updatedArray[index as number].val = value;
       setDataArray(updatedArray);
     } else {
-      valueUpdated(value);
+      updateValue(value);
     }
   };
 
@@ -109,24 +120,39 @@ export const FunctionInput: FC<{
         <Box>
           {dataArray.map((inp, index) => {
             return (
-              <Flex flex="1" alignItems="center" mb="4" key={index}>
+              <Flex
+                flex="1"
+                alignItems="center"
+                mb={index === dataArray.length - 1 ? 0 : 4}
+                key={index}
+              >
                 {getInputComponent(
                   (value: any) => handleUpdate(index, value),
                   index
                 )}
                 {dataArray.length > 1 && (
-                  <Box onClick={() => remove(index)} ml="4">
-                    <CloseIcon name="close" color="red.500" />{' '}
-                  </Box>
+                  <IconButton
+                    variant="link"
+                    colorScheme="red"
+                    onClick={() => remove(index)}
+                    aria-label="add value"
+                    icon={<CloseIcon fontSize=".8rem" />}
+                  />
                 )}
               </Flex>
             );
           })}
+          <Box textAlign="right" alignItems="right">
+            <IconButton
+              py="4"
+              variant="link"
+              colorScheme="green"
+              onClick={add}
+              aria-label="add value"
+              icon={<AddIcon />}
+            />
+          </Box>
         </Box>
-
-        <Link onClick={add} float="right">
-          <AddIcon name="add" color="green.500" />
-        </Link>
       </>
     );
   }
