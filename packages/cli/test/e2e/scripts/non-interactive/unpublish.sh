3c41425cbca5967a17eb47bd4e80d9b2ed74c3d8
saeta-eth <saetaeth@proton.me>
2024-07-18 17:08:10 -0300

    fix

--- packages/cli/test/e2e/scripts/non-interactive/unpublish.sh
+++ packages/cli/test/e2e/scripts/non-interactive/unpublish.sh
@@ -1,1 +1,1 @@
-CANNON_E2E=true $CANNON unpublish greeter-foundry:2.11.18@main --chain-id 1 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --skip-confirm
+CANNON_E2E=true $CANNON unpublish greeter-foundry:2.11.18@main --chain-id 1 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
