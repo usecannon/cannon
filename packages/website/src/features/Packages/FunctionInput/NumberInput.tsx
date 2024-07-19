4d5a01843f33cc6117f5a78ce7a6c5f59a9002f5
saeta.eth <saetaeth@proton.me>
2024-07-19 15:29:41 -0300

    fix(website): Fix zero value input and tuple[] type on Interact page (#1212)
    Co-authored-by: Matías <mjlescano@protonmail.com>

--- packages/website/src/features/Packages/FunctionInput/NumberInput.tsx
+++ packages/website/src/features/Packages/FunctionInput/NumberInput.tsx
@@ -1,26 +1,24 @@
 import { Input } from '@chakra-ui/react';
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { parseEther } from 'viem';
 
 export const NumberInput: FC<{
-  handleUpdate: (value: bigint) => void;
-  value?: string;
-  positiveOnly?: boolean;
-}> = ({ handleUpdate, value = '0', positiveOnly = false }) => {
+  handleUpdate: (value: bigint | undefined) => void;
+  initialValue?: bigint;
+}> = ({ handleUpdate, initialValue }) => {
   // TODO: Doesn't look a solid approach (parseEther if it has dot)
-  const parseValue = (_value = '0'): bigint =>
-    _value
-      ? _value.toString().includes('.')
-        ? parseEther(_value)
-        : BigInt(_value)
-      : BigInt(0);
+  const parseValue = (val = ''): bigint | undefined => {
+    if (!val) return;
+    return val.includes('.') ? parseEther(val) : BigInt(val);
+  };
 
-  const [updateValue, setUpdateValue] = useState<bigint>(parseValue(value));
-  useEffect(() => handleUpdate(updateValue), [updateValue]);
-  const isInvalid = useMemo(
-    () => positiveOnly && updateValue < BigInt(0),
-    [updateValue, positiveOnly]
+  const [currentValue, setUpdateValue] = useState<bigint | undefined>(
+    initialValue
   );
+
+  useEffect(() => {
+    handleUpdate(currentValue || BigInt(0));
+  }, [currentValue]);
 
   return (
     <Input
@@ -28,9 +26,8 @@ export const NumberInput: FC<{
       bg="black"
       step="1"
       size="sm"
-      borderColor={isInvalid ? 'red.500' : 'whiteAlpha.400'}
-      value={updateValue.toString()}
-      _focus={{ borderColor: isInvalid ? 'red.500' : 'blue.300' }}
+      placeholder="0"
+      value={currentValue?.toString() || ''}
       onChange={(e) => {
         setUpdateValue(parseValue(e.target.value));
       }}
