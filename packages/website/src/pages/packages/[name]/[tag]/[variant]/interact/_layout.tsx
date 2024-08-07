f8f6600778d3864ec2203262ea317bd2a7876bb8
nicosampler <nf.dominguez.87@gmail.com>
2024-08-07 09:30:47 -0300

    fix selected tab (header)

--- packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/_layout.tsx
+++ packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/_layout.tsx
@@ -1,18 +1,8 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { useRouter } from 'next/router';
 import InteractTab from '@/features/Packages/Tabs/InteractTab';
 
 export default function InteractLayout({ children }: { children: ReactNode }) {
-  const params = useRouter().query;
-  return (
-    <InteractTab
-      name={decodeURIComponent(params.name as string)}
-      tag={decodeURIComponent(params.tag as string)}
-      variant={decodeURIComponent(params.variant as string)}
-    >
-      {children}
-    </InteractTab>
-  );
+  return <InteractTab>{children}</InteractTab>;
 }
