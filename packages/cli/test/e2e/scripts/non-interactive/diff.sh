3709439747a38508dd6bfd9fc4e1cb3aa2235bcd
Daniel Beal <git@dbeal.dev>
2024-08-14 19:23:25 +0900

    fix tests again

--- packages/cli/test/e2e/scripts/non-interactive/diff.sh
+++ packages/cli/test/e2e/scripts/non-interactive/diff.sh
@@ -1,16 +1,16 @@
 
 # test building the package again with no private key supplied
-$CANNON build $CANNON_REPO_DIR/examples/sample-foundry-project/cannonfile.toml
+$CANNON build $CANNON_REPO_DIR/examples/router-architecture/cannonfile.toml
 
 # the diff command should say there is no diffs immediately after build
-$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/sample-foundry-project
+$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/router-architecture
 
 # lets change one of the files by adding a comment
-echo "# some extra comment" >> $CANNON_REPO_DIR/examples/sample-foundry-project/src/Greeter.sol
+echo "# some extra comment" >> $CANNON_REPO_DIR/examples/router-architecture/src/Greeter.sol
 
 # the diff command should say there is no diffs if we exclude the file
-$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/sample-foundry-project --match-contract lib
-$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/sample-foundry-project --match-source console
+$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/router-architecture --match-contract lib
+$CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/router-architecture --match-source console
 
 # the diff command should say there is a diff if we let it scan everything
-! $CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/sample-foundry-project
+! $CANNON diff greeter-foundry $CANNON_REPO_DIR/examples/router-architecture
