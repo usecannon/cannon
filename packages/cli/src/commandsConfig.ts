970c7bede27d10bda00310b11ca8435786dd950b
FuzzB0t <christopherpagnusatt@gmail.com>
2024-08-21 15:32:55 -0600

    lint fixes

--- packages/cli/src/commandsConfig.ts
+++ packages/cli/src/commandsConfig.ts
@@ -466,10 +466,13 @@ const commandsConfig = {
         description: 'IPFS hash to write deployment data for',
       },
     ],
-    options: [...debugVerbosity, {
-      flags: '-c --chain-id <chainId>',
-      description: 'Chain ID of deployment to pin',
-    }],
+    options: [
+      ...debugVerbosity,
+      {
+        flags: '-c --chain-id <chainId>',
+        description: 'Chain ID of deployment to pin',
+      },
+    ],
   },
   publish: {
     description:
