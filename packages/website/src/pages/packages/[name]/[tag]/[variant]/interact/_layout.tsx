75419128410b1c4e7929b13bc122992452514df4
nicosampler <nf.dominguez.87@gmail.com>
2024-08-27 15:14:27 -0300

    fix ganache dynamic import

--- packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/_layout.tsx
+++ packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/_layout.tsx
@@ -2,7 +2,18 @@
 
 import { ReactNode } from 'react';
 import InteractTab from '@/features/Packages/Tabs/InteractTab';
+import { useRouter } from 'next/router';
+import PageLoading from '@/components/PageLoading';
+
+function WrapperInteractLayout({ children }: { children: ReactNode }) {
+  return <InteractTab>{children}</InteractTab>;
+}
 
 export default function InteractLayout({ children }: { children: ReactNode }) {
-  return <InteractTab>{children}</InteractTab>;
+  const router = useRouter();
+  return router.isReady ? (
+    <WrapperInteractLayout>{children}</WrapperInteractLayout>
+  ) : (
+    <PageLoading />
+  );
 }
