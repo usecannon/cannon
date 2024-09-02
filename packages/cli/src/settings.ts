fcaf17a859977e4581bfc66f0c43ca295657a566
saeta.eth <saetaeth@proton.me>
2024-09-02 14:31:50 -0300

    fix(cli): display of IPFS loader label (#1323)

--- packages/cli/src/settings.ts
+++ packages/cli/src/settings.ts
@@ -2,7 +2,7 @@ import Debug from 'debug';
 import fs from 'fs-extra';
 import _ from 'lodash';
 import path from 'path';
-import { yellow, bold } from 'chalk';
+import { yellowBright, bold } from 'chalk';
 import untildify from 'untildify';
 import * as viem from 'viem';
 import { parseEnv } from 'znv';
@@ -137,7 +137,7 @@ export const RPC_URL_DEFAULT = 'frame,direct';
 
 const deprecatedWarn = _.once((deprecatedFlag: string, newFlag: string) => {
   log();
-  warn(yellow(bold(`The ${deprecatedFlag} option will be deprecated soon. Use ${newFlag} instead.`)));
+  warn(yellowBright(bold(`⚠️ The ${deprecatedFlag} option will be deprecated soon. Use ${newFlag} instead.`)));
   log();
 });
 
