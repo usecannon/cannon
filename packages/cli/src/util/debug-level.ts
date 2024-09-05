216c8d7756030f5c89d55aa89bcb836a23a97f06
saeta-eth <saetaeth@proton.me>
2024-09-05 12:22:05 -0300

    fix lint issues

--- packages/cli/src/util/debug-level.ts
+++ packages/cli/src/util/debug-level.ts
@@ -1,22 +1,22 @@
-import Debug from "debug";
+import Debug from 'debug';
 
 /**
  * Sets the debug level based on the provided options.
  * @param opts Options to define debug level.
  */
 export const setDebugLevel = (opts: any) => {
-    switch (true) {
-      case opts.Vvvv:
-        Debug.enable('cannon:*');
-        break;
-      case opts.Vvv:
-        Debug.enable('cannon:builder*');
-        break;
-      case opts.Vv:
-        Debug.enable('cannon:builder,cannon:builder:definition');
-        break;
-      case opts.v:
-        Debug.enable('cannon:builder');
-        break;
-    }
+  switch (true) {
+    case opts.Vvvv:
+      Debug.enable('cannon:*');
+      break;
+    case opts.Vvv:
+      Debug.enable('cannon:builder*');
+      break;
+    case opts.Vv:
+      Debug.enable('cannon:builder,cannon:builder:definition');
+      break;
+    case opts.v:
+      Debug.enable('cannon:builder');
+      break;
   }
+}
