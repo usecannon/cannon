da4b5731694453e955aadf9ede6eaa9c42c4fa10
Matías <mjlescano@protonmail.com>
2024-10-02 20:45:08 -0300

    remove console.log

--- packages/cli/src/foundry.ts
+++ packages/cli/src/foundry.ts
@@ -108,8 +108,6 @@ export async function getFoundryArtifact(name: string, baseDir = '', includeSour
     debug('detected foundry info', artifact.metadata);
     debug('evm version', evmVersionInfo);
 
-    console.log({ artifact });
-
     const solcVersion = artifact.metadata.compiler.version;
     const sources = _.mapValues(artifact.metadata.sources, (v, sourcePath) => {
       return {
