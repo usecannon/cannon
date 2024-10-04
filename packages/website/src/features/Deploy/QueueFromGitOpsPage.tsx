2a6dc65e2ddcbcd8f4b6edf17d0322bde5118087
Matías <mjlescano@protonmail.com>
2024-10-04 12:44:45 -0300

    fix(website): existing contract deployment

--- packages/website/src/features/Deploy/QueueFromGitOpsPage.tsx
+++ packages/website/src/features/Deploy/QueueFromGitOpsPage.tsx
@@ -548,8 +548,8 @@ export default function QueueFromGitOps() {
     cannonfileUrlInput &&
       cannonDefInfo?.def &&
       !hasDeployers &&
-      cannonDefInfo.def.allActionNames.some((item) =>
-        item.startsWith('deploy.')
+      cannonDefInfo.def.allActionNames.some(
+        (item) => item.startsWith('deploy.') || item.startsWith('contract.')
       )
   );
 
