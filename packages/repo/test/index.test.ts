63b509b3460a76560f5d5c7c6e1c7ba488df8d8e
Matías <mjlescano@protonmail.com>
2024-10-16 22:26:37 -0300

    remove unnecessart await

--- packages/repo/test/index.test.ts
+++ packages/repo/test/index.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
 import { describe, before, it, TestContext } from 'node:test';
 import axios, { AxiosInstance } from 'axios';
 import { uncompress } from '@usecannon/builder/dist/src/ipfs';
@@ -5,7 +7,7 @@ import { repoServerMock } from './helpers/repo-server';
 import { ipfsServerMock } from './helpers/ipfs-server-mock';
 import { redisServerMock } from './helpers/redis-server-mock';
 
-void describe('POST /api/v0/cat', async function () {
+describe('POST /api/v0/cat', async function () {
   let repo: AxiosInstance;
 
   before(async function () {
@@ -17,19 +19,19 @@ void describe('POST /api/v0/cat', async function () {
     });
   });
 
-  await it('should return 400 on missing ipfshash', async function (t: TestContext) {
+  it('should return 400 on missing ipfshash', async function (t: TestContext) {
     const res = await repo.post('/api/v0/cat');
     t.assert.strictEqual(res.status, 400);
     t.assert.strictEqual(res.data, 'argument "ipfs-path" is required');
   });
 
-  await it('should return 404 on unregistered ipfshash', async function (t: TestContext) {
+  it('should return 404 on unregistered ipfshash', async function (t: TestContext) {
     const res = await repo.post('/api/v0/cat?arg=QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB');
     t.assert.strictEqual(res.status, 404);
     t.assert.strictEqual(res.data, 'unregistered ipfs data');
   });
 
-  await it('should return a pinned file that is not registered', async function (t: TestContext) {
+  it('should return a pinned file that is not registered', async function (t: TestContext) {
     const res = await repo.post(
       '/api/v0/cat?arg=QmYrLsLZwGGg68XwGkdrx8q9KaRSHq7e5sa6PbT7kNXjKw',
       {},
