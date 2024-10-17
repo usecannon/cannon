a131f122eb27151ad3f18f829e2817c70fe79647
Matías <mjlescano@protonmail.com>
2024-10-16 22:10:23 -0300

    add first cases for cat

--- packages/repo/test/helpers/ipfs-server-mock.ts
+++ packages/repo/test/helpers/ipfs-server-mock.ts
@@ -1,7 +1,11 @@
-import { createServer, IncomingMessage, ServerResponse, Server } from 'node:http';
 import { after } from 'node:test';
+import { Server } from 'node:http';
+import fs, { promises as fsPromises } from 'node:fs';
+import path from 'node:path';
 import { promisify } from 'node:util';
-import getPort from 'get-port';
+import express from 'express';
+import { getPort } from 'get-port-please';
+import { compress, getContentCID } from '@usecannon/builder/dist/src/ipfs';
 
 const servers: Server[] = [];
 
@@ -9,23 +13,40 @@ after(async function () {
   await Promise.all(servers.map((server) => promisify(server.close.bind(server))()));
 });
 
-const handleRequest = (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      data: 'Hello World!',
-    })
-  );
-};
-
 export async function ipfsServerMock() {
   const port = await getPort();
 
-  const server = createServer(handleRequest);
+  const app = express();
+
+  app.post('/api/v0/cat', async (req, res) => {
+    const cid = req.query.arg;
+
+    if (!cid) {
+      return res.status(400).end('invalid cid');
+    }
+
+    const filepath = path.resolve(__dirname, '..', 'fixtures', `${cid}.json`);
+
+    if (!fs.existsSync(filepath)) {
+      return res.status(404).end('file not found on fixtures');
+    }
+
+    const content = await fsPromises.readFile(filepath);
+    const data = Buffer.from(compress(JSON.stringify(JSON.parse(content.toString()))));
+
+    const resultCid = await getContentCID(data);
+    if (resultCid !== cid) {
+      throw new Error(`Result cid "${resultCid}" !== "${cid}"`);
+    }
+
+    res.send(data).end();
+  });
+
+  const server = await new Promise<Server>((resolve) => {
+    const _server = app.listen(port, () => resolve(_server));
+  });
 
   servers.push(server);
-
-  server.listen(port);
 
   return { ipfsUrl: `http://127.0.0.1:${port}` };
 }
---
+++ packages/repo/test/helpers/repo-server.ts
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import { getPort } from 'get-port-please';
+import { spawn } from './spawn';
+
+// PORT=8328 REDIS_URL=redis://... UPSTREAM_IPFS_URL=http://localhost:9095 node dist/index.js
+export async function repoServerMock({ redisUrl, ipfsUrl }: { redisUrl: string; ipfsUrl: string }) {
+  const port = await getPort();
+
+  const serverPath = path.resolve(__dirname, '..', '..', 'src', 'index.ts');
+
+  const proc = spawn('pnpm', ['ts-node', serverPath], {
+    PORT: port.toString(),
+    REDIS_URL: redisUrl,
+    UPSTREAM_IPFS_URL: ipfsUrl,
+  });
+
+  for await (const line of proc) {
+    if (line.includes('listening on port ')) break;
+  }
+
+  return { repoUrl: `http://127.0.0.1:${port}` };
+}
