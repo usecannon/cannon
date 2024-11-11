fa860aa9a88080c56ba21a20b16a7215782a7004
Daniel Beal <git@dbeal.dev>
2024-11-12 00:45:58 +0900

    fix(builder): diamond proxy edge cases
    * `OwnershipFacet` now has renamed functions to make it diamond specific
    * `OwnershipFacet` not included in immutable deploys
    * if an error occurs in  the diamond upgrade, its now decoded with very readable errors (see below)

--- packages/builder/src/steps/diamond.ts
+++ packages/builder/src/steps/diamond.ts
@@ -158,7 +158,7 @@ const diamondStep = {
     debug('first time deploy? ', !!ctx.contracts[stepName]);
     let outputContracts = ctx.contracts[stepName] ? ctx.contracts : await firstTimeDeploy(runtime, config, packageState);
     const deployedContracts = config.immutable
-      ? [stepName + 'OwnershipFacet', stepName + 'DiamondLoupeFacet']
+      ? [stepName + 'DiamondLoupeFacet']
       : [
           stepName + 'OwnershipFacet',
           stepName + 'DiamondLoupeFacet',
@@ -166,7 +166,10 @@ const diamondStep = {
           stepName + 'DiamondWipeAndPaveFacet',
         ];
     outputContracts = _.pick(outputContracts, [stepName, ...deployedContracts]);
-    debug('output contracts', deployedContracts);
+    debug('output contracts', [stepName, ...deployedContracts]);
+
+    // ensure that diamond errors can be decoded if we run into an error on the next step(s)
+    runtime.updateProviderArtifacts({ contracts: outputContracts });
 
     const proxyAddress = outputContracts[stepName].address;
 
