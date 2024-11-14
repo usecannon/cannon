e35713e2893a001e537abddd237bc65446c6427c
saeta.eth <saetaeth@proton.me>
2024-11-14 08:26:26 -0300

    fix(website): Resolve "Get Started" page not found error (#1547)

--- packages/website/src/pages/learn/guides/get-started/[section]/index.tsx
+++ packages/website/src/pages/learn/guides/get-started/[section]/index.tsx
@@ -1,5 +1,4 @@
-import { NextPageContext } from 'next';
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 import { NextSeo } from 'next-seo';
 import { allGuides, type Guides } from 'contentlayer/generated';
 import defaultSEO from '@/constants/defaultSeo';
@@ -8,8 +7,21 @@ import { DocsNav } from '@/components/docs';
 import Custom404 from '@/pages/404';
 import Layout from '../../../_layout';
 import NestedLayout from '../../guideLayout';
+import { useRouter } from 'next/router';
 
-export default function GetStarted({ guide }: { guide: Guides }) {
+export default function GetStarted() {
+  const [guide, setGuide] = useState<Guides | null>(null);
+  const { asPath } = useRouter();
+
+  useEffect(() => {
+    const guide = allGuides.find((guide: Guides) => {
+      return guide.url.split('/').pop() === asPath.split('/').pop();
+    });
+
+    if (!guide) setGuide(null);
+    else setGuide(guide);
+  }, [asPath]);
+
   return (
     <>
       <NextSeo
@@ -35,14 +47,6 @@ export default function GetStarted({ guide }: { guide: Guides }) {
     </>
   );
 }
-
-GetStarted.getInitialProps = async (ctx: NextPageContext) => {
-  const guide = await allGuides.find(
-    (guide: Guides) => guide._raw.flattenedPath === ctx.query.section
-  );
-
-  return { guide: guide };
-};
 
 GetStarted.getLayout = function getLayout(page: ReactElement) {
   return (
