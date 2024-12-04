5d7761f3a95f5421a14a1dd815d60cd321a2d717
Matías <mjlescano@protonmail.com>
2024-12-04 11:05:34 +0900

    update add tests

--- packages/repo/test/add.test.ts
+++ packages/repo/test/add.test.ts
@@ -1,43 +1,36 @@
 import { describe, it, TestContext } from 'node:test';
 import { uncompress, prepareFormData } from '../../builder/src/ipfs';
 import { bootstrap } from './helpers/bootstrap';
-import { assertRes } from './helpers/assert-res';
 import { loadFixture } from './helpers/fixtures';
 
 describe('POST /api/v0/add', async function () {
   const ctx = bootstrap();
 
-  it.only('should return 400 when no data is provided', async function (t: TestContext) {
-    const res = await ctx.repo.post('/api/v0/add');
-
-    assertRes(t, res, {
-      status: 400,
-      data: 'no upload data',
-    });
+  it.only('should return 400 when no data is provided', async function () {
+    await ctx.repo.post('/api/v0/add').expect(400, 'no upload data');
   });
 
-  it('should return 400 when trying to add non cannon package', async function (t: TestContext) {
+  it('should return 400 when trying to add non cannon package', async function () {
     const { content } = await loadFixture('greeter-misc');
     const { formData } = await prepareFormData(content);
 
-    const res = await ctx.repo.post('/api/v0/add', formData);
-
-    assertRes(t, res, {
-      status: 400,
-      data: 'does not appear to be cannon package',
-    });
+    await ctx.repo
+      .post('/api/v0/add')
+      .attach('file', formData.get('file'))
+      .expect(400, 'does not appear to be cannon package');
   });
 
   it('should successfully add valid package data', async function (t: TestContext) {
     const pkg = await loadFixture('registry');
     const pkgData = await prepareFormData(pkg.content);
 
-    const res = await ctx.repo.post('/api/v0/add', pkgData.formData);
-
-    assertRes(t, res, {
-      status: 200,
-      data: { Hash: pkg.cid },
-    });
+    await ctx.repo
+      .post('/api/v0/add')
+      .attach('file', pkgData.formData.get('file'))
+      .expect(200)
+      .expect((res) => {
+        t.assert.deepStrictEqual(JSON.parse(res.text), { Hash: pkg.cid });
+      });
 
     const saved = JSON.parse(uncompress(ctx.ipfsMockGet(pkg.cid)));
     t.assert.deepStrictEqual(saved, pkg.content);
@@ -45,12 +38,14 @@ describe('POST /api/v0/add', async function () {
     // After adding the package, we should also be able to add the misc data
     const misc = await loadFixture('registry-misc');
     const miscData = await prepareFormData(misc.content);
-    const miscRes = await ctx.repo.post('/api/v0/add', miscData.formData);
 
-    assertRes(t, miscRes, {
-      status: 200,
-      data: { Hash: misc.cid },
-    });
+    await ctx.repo
+      .post('/api/v0/add')
+      .attach('file', miscData.formData.get('file'))
+      .expect(200)
+      .expect((res) => {
+        t.assert.deepStrictEqual(JSON.parse(res.text), { Hash: misc.cid });
+      });
 
     const files = await ctx.s3List();
     console.log(files);
@@ -60,16 +55,26 @@ describe('POST /api/v0/add', async function () {
     const { cid, content } = await loadFixture('owned-greeter');
     const { formData } = await prepareFormData(content);
 
-    const res1 = await ctx.repo.post('/api/v0/add', formData);
-    assertRes(t, res1, { status: 200, data: { Hash: cid } });
+    await ctx.repo
+      .post('/api/v0/add')
+      .attach('file', formData.get('file'))
+      .expect(200)
+      .expect((res) => {
+        t.assert.deepStrictEqual(JSON.parse(res.text), { Hash: cid });
+      });
 
     const saved = JSON.parse(uncompress(ctx.ipfsMockGet(cid)));
     t.assert.deepStrictEqual(saved, content);
 
     const { formData: formData2 } = await prepareFormData(content);
 
-    const res2 = await ctx.repo.post('/api/v0/add', formData2);
-    assertRes(t, res2, { status: 200, data: { Hash: cid } });
+    await ctx.repo
+      .post('/api/v0/add')
+      .attach('file', formData2.get('file'))
+      .expect(200)
+      .expect((res) => {
+        t.assert.deepStrictEqual(JSON.parse(res.text), { Hash: cid });
+      });
 
     const saved2 = JSON.parse(uncompress(ctx.ipfsMockGet(cid)));
     t.assert.deepStrictEqual(saved2, content);
