b9ce40fcf11189612391d8f6eb3a57f0462feb60
Daniel Beal <git@dbeal.dev>
2024-12-10 21:47:07 +0900

    remove stable stringify due to slowness; switch to more reliable diffing method

--- packages/builder/src/builder.ts
+++ packages/builder/src/builder.ts
@@ -129,19 +129,28 @@ ${printChainDefinitionProblems(problems)}`);
         runtime.reportOperatingContext(n, ctx);
 
         try {
-          const curHashes = await def.getState(n, runtime, ctx, depsTainted);
+          let match = state[n] && state[n].hash !== 'SKIP';
+          if (state[n] && !match) {
+            for await (const hash of def.getState(n, runtime, ctx, depsTainted)) {
+              debug('comparing states', state[n] ? state[n].hash : null, hash);
 
-          debug('comparing states', state[n] ? state[n].hash : null, curHashes);
-          if (!state[n] || (state[n].hash !== 'SKIP' && curHashes && !curHashes.includes(state[n].hash || ''))) {
+              if (state[n].hash !== 'SKIP' && hash === state[n].hash) {
+                match = true;
+                break;
+              }
+            }
+          }
+
+          if (!match) {
             debug('run isolated', n);
             const newArtifacts = await runStep(runtime, { ref, currentLabel: n }, def.getConfig(n, ctx), ctx);
 
             // some steps may be self introspective, causing a step to be giving the wrong hash initially. to counteract this, we recompute the hash
             addOutputsToContext(ctx, newArtifacts);
-            const newStates = await def.getState(n, runtime, ctx, depsTainted);
+            const newState = await def.getState(n, runtime, ctx, depsTainted).next();
             state[n] = {
               artifacts: newArtifacts,
-              hash: newStates && newStates.length ? newStates[0] : null,
+              hash: newState.value,
               version: BUILD_VERSION,
             };
             tainted.add(n);
@@ -241,12 +250,22 @@ export async function buildLayer(
     runtime.reportOperatingContext(action, ctx);
 
     try {
-      const curHashes = await def.getState(action, runtime, ctx, false);
-
       if (isCompleteLayer) {
-        debug('comparing layer states', state[action] ? state[action].hash : null, curHashes);
-        if (!state[action] || (curHashes && !curHashes.includes(state[action].hash || ''))) {
+        let match = state[action] && state[action].hash !== 'SKIP';
+        if (state[action] && !match) {
+          for await (const hash of def.getState(action, runtime, ctx, false)) {
+            debug('comparing layer states', state[action] ? state[action].hash : null, hash);
+
+            if (state[action].hash !== 'SKIP' && hash === state[action].hash) {
+              match = true;
+              break;
+            }
+          }
+        }
+
+        if (!match) {
           debug('operation', action, 'in layer needs to be rebuilt');
+
           isCompleteLayer = false;
           break;
         }
@@ -311,10 +330,10 @@ export async function buildLayer(
 
       addOutputsToContext(ctx, newArtifacts);
 
-      const newHashes = await def.getState(action, runtime, ctx, false);
+      const newHash = (await def.getState(action, runtime, ctx, false).next()).value;
       state[action] = {
         artifacts: newArtifacts,
-        hash: newHashes && newHashes.length ? newHashes[0] : null,
+        hash: newHash,
         version: BUILD_VERSION,
         // add the chain dump later once all steps have been executed
       };
