4fcf69b9d08056366e2f1698df1f4f0601dfbe4a
Matías <mjlescano@protonmail.com>
2025-01-09 17:54:46 -0300

    add base contracts for subscription

---
+++ packages/registry/contracts/CannonSubscription.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {SetUtil} from "@synthetixio/core-contracts/contracts/utils/SetUtil.sol";
+import {OwnedUpgradableUpdated} from "./OwnedUpgradableUpdated.sol";
+import {Subscription} from "./Subscription.sol";
+
+/**
+ * @title Management of Subscriptions to Cannon Registry
+ */
+contract CannonSubscription {
+}
