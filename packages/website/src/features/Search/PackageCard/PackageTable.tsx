00f6163d682908fde3e8b4f1e76c9a7ac6285532
AkihisaY <76488710+AkihisaY@users.noreply.github.com>
2025-01-14 22:05:57 +0900

    fix(website): layout change for mobile (#1626)

--- packages/website/src/features/Search/PackageCard/PackageTable.tsx
+++ packages/website/src/features/Search/PackageCard/PackageTable.tsx
@@ -53,7 +53,9 @@ const PackageTable: FC<{
   ];
 
   if (latestOnly) {
-    data = data.filter((row: any) => row.version === 'latest');
+    data = data.filter(
+      (row: any) => row.version === 'latest' && row.chain !== 13370
+    );
 
     data = data.filter((row: any) => {
       const matchingChain = getChainById(row.chain);
