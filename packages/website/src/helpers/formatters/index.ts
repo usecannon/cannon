b2b0b91716bbaa959f99c06cf2ee614f0cb001e8
nicolas <nicosampler@users.noreply.github.com>
2025-02-21 14:04:20 -0300

    fix(website): CAN-716 (#1711)

--- packages/website/src/helpers/formatters/index.ts
+++ packages/website/src/helpers/formatters/index.ts
@@ -3,3 +3,7 @@ import { formatUnits } from 'viem';
 export const formatToken = (amount: bigint, options?: { decimals?: number; symbol?: string }) => {
   return `${formatUnits(amount, options?.decimals || 18)} ${options?.symbol}`.trim();
 };
+
+export const formatTransactionHash = (hash: string) => {
+  return `${hash.slice(0, 6)}...${hash.slice(-4)}`;
+};
