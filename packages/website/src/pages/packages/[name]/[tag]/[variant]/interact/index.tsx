284553c0af168c802b2fe8ee10014fa3bae71f0f
nicosampler <nf.dominguez.87@gmail.com>
2025-03-07 14:29:06 -0300

    refactor(website): Interact 1

--- packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/index.tsx
+++ packages/website/src/pages/packages/[name]/[tag]/[variant]/interact/index.tsx
@@ -4,7 +4,6 @@ import { NextSeo } from 'next-seo';
 import defaultSEO from '@/constants/defaultSeo';
 import { usePackageNameTagVersionUrlParams } from '@/hooks/routing/usePackageVersionUrlParams';
 import { useCannonChains } from '@/providers/CannonProvidersProvider';
-import InteractTab from '@/features/Packages/Tabs/InteractTab';
 
 function generateMetadata({
   name,
@@ -52,23 +51,22 @@ export default function Interact() {
   });
 
   return (
-    <NextSeo
-      {...defaultSEO}
-      title={metadata.title}
-      description={metadata.description}
-      openGraph={{
-        ...defaultSEO.openGraph,
-        title: metadata.title,
-        description: metadata.description,
-      }}
-    />
+    <>
+      <NextSeo
+        {...defaultSEO}
+        title={metadata.title}
+        description={metadata.description}
+        openGraph={{
+          ...defaultSEO.openGraph,
+          title: metadata.title,
+          description: metadata.description,
+        }}
+      />
+      <Interact />
+    </>
   );
 }
 
 Interact.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <NameTagVariantLayout>
-      <InteractTab>{page}</InteractTab> {/* Redirects to [moduleName] */}
-    </NameTagVariantLayout>
-  );
+  return <NameTagVariantLayout>{page}</NameTagVariantLayout>;
 };
