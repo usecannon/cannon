406f198d314c02f8d0c71cb294d1f524d2633777
dbeal <git@danb.email>
2025-03-19 21:10:33 -0700

    Update rpc.ts

--- packages/website/src/helpers/rpc.ts
+++ packages/website/src/helpers/rpc.ts
@@ -8,7 +8,7 @@ async function loadGanache() {
   // More info: https://stackoverflow.com/questions/49475492/npm-install-error-code-ebadplatform
 
   // @ts-ignore-next-line Import module
-  await import('./ganache.min.js');
+  await import('./ganache/ganache.min.js');
   return new Promise((resolve) => {
     const checkGanache = setInterval(() => {
       if (window.Ganache) {
