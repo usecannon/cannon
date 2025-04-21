3b814f93f2dbff3471358604f11188625239f88f
Matías <mjlescano@users.noreply.github.com>
2025-04-21 18:56:29 -0300

    fix(website): Interact search box (#1764)

---
+++ packages/website/src/features/Packages/interact/useContractSearch.ts
@@ -0,0 +1,27 @@
+import { useEffect, useMemo, useState } from 'react';
+import Fuse from 'fuse.js';
+import { Option } from '@/lib/interact';
+
+export function useContractSearch(options: Option[]) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState<Option[]>([]);
+
+  const fuse = useMemo(
+    () =>
+      new Fuse<Option>(options, {
+        keys: ['moduleName', 'contractName', 'contractAddress'],
+      }),
+    [options]
+  );
+
+  useEffect(() => {
+    if (searchTerm) {
+      const results = fuse.search(searchTerm).map(({ item }) => item);
+      setSearchResults(results);
+    } else {
+      setSearchResults(options);
+    }
+  }, [fuse, searchTerm, options]);
+
+  return { searchTerm, setSearchTerm, searchResults };
+}
