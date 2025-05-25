7ae69ed2e49e41c86b32889c5e61738ea9d101da
Daniel Beal <git@dbeal.dev>
2025-05-25 11:52:52 +0900

    setup 4byte directory scanning

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,4 +1,6 @@
 import { loop as registryLoop } from './registry';
+import { loop as signatureDirectoryLoop } from './4byte-directory';
 export * from './db';
 
 void registryLoop();
+void signatureDirectoryLoop();
