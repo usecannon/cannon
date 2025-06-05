fd092f55b39ccbfb2cec82b2824d1c67fd898b68
nicolas <nicosampler@users.noreply.github.com>
2025-06-05 17:52:44 -0300

    fix(website): CAN-748 (#1792)

--- packages/website/src/features/Packages/interact/useActiveContract.ts
+++ packages/website/src/features/Packages/interact/useActiveContract.ts
@@ -1,24 +1,29 @@
 import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
 /**
  * Custom hook to get the active contract information from the URL
+ * Returns memoized contract information to prevent unnecessary recalculations
  */
 export function useActiveContract() {
   const pathName = useRouter().asPath;
-  const withoutMethodSelector = pathName.split('#')[0];
-  const selectedContractInfo = withoutMethodSelector.split('interact/')[1];
 
-  if (selectedContractInfo) {
-    const [moduleName, contractName, contractAddress] = selectedContractInfo.split('/');
+  return useMemo(() => {
+    const withoutMethodSelector = pathName.split('#')[0];
+    const selectedContractInfo = withoutMethodSelector.split('/interact/')[1];
 
-    if (!moduleName || !contractName || !contractAddress) {
-      return undefined;
+    if (selectedContractInfo) {
+      const [moduleName, contractName, contractAddress] = selectedContractInfo.split('/');
+
+      if (!moduleName || !contractName || !contractAddress) {
+        return undefined;
+      }
+
+      return {
+        moduleName,
+        contractName,
+        contractAddress,
+      };
     }
-
-    return {
-      moduleName,
-      contractName,
-      contractAddress,
-    };
-  }
+  }, [pathName]);
 }
