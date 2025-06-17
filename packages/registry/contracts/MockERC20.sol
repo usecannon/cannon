85d40d5c5dfea16db838698b4e6897e1722a967d
Daniel Beal <git@dbeal.dev>
2025-06-17 14:08:17 +0900

    fix lint

--- packages/registry/contracts/MockERC20.sol
+++ packages/registry/contracts/MockERC20.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.24;
 import {ERC20} from "@synthetixio/core-contracts/contracts/token/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor() {
-        _initialize("ERC20Mock", "E20M", 18);
-    }
+  constructor() {
+    _initialize("ERC20Mock", "E20M", 18);
+  }
 
-    function mint(address account, uint256 amount) external {
-        _mint(account, amount);
-    }
+  function mint(address account, uint256 amount) external {
+    _mint(account, amount);
+  }
 
-    function burn(address account, uint256 amount) external {
-        _burn(account, amount);
-    }
+  function burn(address account, uint256 amount) external {
+    _burn(account, amount);
+  }
 }
