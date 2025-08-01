05080db4ceb8ed6f88c7f627f4f41b00f239725e
Daniel Beal <git@dbeal.dev>
2025-08-01 17:01:34 +0900

    try again with the no-constant-condition shutdown

--- packages/indexer/src/4byte-directory.ts
+++ packages/indexer/src/4byte-directory.ts
@@ -48,7 +48,7 @@ export async function loop() {
 
   console.log('start signature database scan loop');
 
-  /* @eslint no-constant-condition: "off" */
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     let nextUrl: string | null = 'https://www.4byte.directory/api/v1/signatures/?format=json';
     let failureCount = 0;
