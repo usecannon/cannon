dd3ac82a7d8df3591d463108172b3916fe4de248
AkihisaY <76488710+AkihisaY@users.noreply.github.com>
2025-09-18 15:42:11 +0900

    fix(website): filters to package list (#1836)

--- packages/website/src/features/Search/PackageCard/PackageCard.tsx
+++ packages/website/src/features/Search/PackageCard/PackageCard.tsx
@@ -3,6 +3,8 @@ import PackageTable from './PackageTable';
 import Link from 'next/link';
 import { FC } from 'react';
 import { ApiDocument } from '@usecannon/api/dist/src/types';
+import React, { useState } from 'react';
+import SearchInput from '@/components/SearchInput';
 
 interface IPackageCardProps {
   pkgs: ApiDocument[];
@@ -10,13 +12,15 @@ interface IPackageCardProps {
 }
 
 export const PackageCard: FC<IPackageCardProps> = ({ pkgs, maxHeight }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
   return (
     <div
       key={pkgs[0].name}
       className="block overflow-hidden border border-border rounded bg-black transition-all duration-120"
     >
-      <div className="flex flex-row items-center p-2">
-        <div className="px-1 flex items-center">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between w-full p-2 space-y-2 sm:space-y-0">
+        <div className="px-1 flex flex-wrap items-center">
           <h4 className="font-semibold text-lg">{pkgs[0].name}</h4>
           <Link
             href={'/packages/' + pkgs[0].name}
@@ -25,10 +29,11 @@ export const PackageCard: FC<IPackageCardProps> = ({ pkgs, maxHeight }) => {
             <LinkIcon className="w-4 h-4" />
           </Link>
         </div>
+        <SearchInput size="sm" onSearchChange={setSearchTerm} />
       </div>
 
       <div className="align-middle overflow-auto" style={{ maxHeight }}>
-        <PackageTable latestOnly={false} pkgs={pkgs} />
+        <PackageTable latestOnly={false} pkgs={pkgs} searchTerm={searchTerm} />
       </div>
     </div>
   );
